0945dd3605149e908947479edfdda9fc9f6c66df
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-02-06 17:13:03 +0200

    Add CanFloatWidgetOptions.

--- ui/appui-react/src/appui-react/widgets/Widget.tsx
+++ ui/appui-react/src/appui-react/widgets/Widget.tsx
@@ -11,6 +11,14 @@ import { IconSpec, SizeProps } from "@itwin/core-react";
 import { ProviderItem } from "../ui-items-provider/ProviderItem";
 import { WidgetState } from "./WidgetState";
 
+interface CanFloatWidgetOptions {
+  readonly isResizable?: boolean;
+  readonly defaultPosition?: PointProps;
+  readonly defaultSize?: SizeProps;
+  readonly containerId?: string;
+  readonly hideWithUi?: boolean;
+}
+
 /** Describes the data needed to provide a widget.
  * @public // TODO: 4.x cleanup
  */
@@ -19,16 +27,11 @@ export interface Widget extends ProviderItem {
   readonly allowedPanels?: ReadonlyArray<StagePanelLocation>;
   readonly badgeType?: BadgeType;
   readonly canPopout?: boolean;
-  readonly defaultFloatingPosition?: PointProps;
-  readonly defaultFloatingSize?: SizeProps;
+  readonly canFloat?: boolean | CanFloatWidgetOptions;
   readonly defaultState?: WidgetState;
-  readonly floatingContainerId?: string;
   /** A React element for the Widget. */
   readonly element?: React.ReactNode;
-  readonly hideWithUiWhenFloating?: boolean;
   readonly icon?: IconSpec;
-  readonly isFloatingStateSupported?: boolean;
-  readonly isFloatingStateWindowResizable?: boolean;
   readonly label?: string | ConditionalStringValue;
   readonly priority?: number;
   readonly tooltip?: string | ConditionalStringValue;
