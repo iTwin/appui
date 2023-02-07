224a89178cbe06cc60b6676394eec1b266a76668
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-02-07 10:46:44 +0200

    Update UiItemsProvider.

--- ui/appui-react/src/appui-react/ui-items-provider/UiItemsProvider.ts
+++ ui/appui-react/src/appui-react/ui-items-provider/UiItemsProvider.ts
@@ -11,25 +11,23 @@ import { StagePanelLocation } from "../stagepanels/StagePanelLocation";
 import { StagePanelSection } from "../stagepanels/StagePanelSection";
 import { StatusBarItem } from "../statusbar/StatusBarItem";
 import { ToolbarItem, ToolbarOrientation, ToolbarUsage } from "../toolbar/ToolbarItem";
-import { WidgetProps } from "../widgets/WidgetProps";
+import { Widget } from "../widgets/Widget";
 
 /** Describes interface of objects that want to provide UI component to the running IModelApp.
  * @beta
  */
 export interface UiItemsProvider {
-  /** id of provider */
+  /** Id of provider. */
   readonly id: string;
 
-  /** UiItemsManager calls following method to get items to populate specific toolbars */
-  provideToolbarButtonItems?: (stageId: string, stageUsage: string, toolbarUsage: ToolbarUsage, toolbarOrientation: ToolbarOrientation) => ToolbarItem[];
-  /** UiItemsManager calls following method to augment base statusbar for stages that allow it. */
-  provideStatusBarItems?: (stageId: string, stageUsage: string) => StatusBarItem[];
-  /** UiItemsManager calls following method to augment backstage items. */
-  provideBackstageItems?: () => BackstageItem[];
-  /** UiItemsManager calls following method to augment Widget lists.
-   * @note Returned widgets must provide unique `AbstractWidgetProps["id"]` to correctly save/restore App layout.
-   */
-  provideWidgets?: (stageId: string, stageUsage: string, location: StagePanelLocation, section?: StagePanelSection) => ReadonlyArray<WidgetProps>;
-  /** Function called when the provider is unregistered via `ItemsManager.unregister` to allow provider to do cleanup. */
-  onUnregister?: () => void;
+  /** Provides toolbar items. */
+  readonly provideToolbarItems?: (stageId: string, stageUsage: string, toolbarUsage: ToolbarUsage, toolbarOrientation: ToolbarOrientation) => ReadonlyArray<ToolbarItem>;
+  /** Provides status bar items. */
+  readonly provideStatusBarItems?: (stageId: string, stageUsage: string) => ReadonlyArray<StatusBarItem>;
+  /** Provides backstage items. */
+  readonly provideBackstageItems?: () => ReadonlyArray<BackstageItem>;
+  /** Provides widgets. */
+  readonly provideWidgets?: (stageId: string, stageUsage: string, location: StagePanelLocation, section?: StagePanelSection) => ReadonlyArray<Widget>;
+  /** Function called when the provider is unregistered. Allows provider to do a cleanup. */
+  readonly onUnregister?: () => void;
 }
