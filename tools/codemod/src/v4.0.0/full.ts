2c7b813f960324277a9972ba6de2988431f74a32
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-03-02 22:25:10 +0200

    Add abstract codemod.

--- tools/codemod/src/v4.0.0/full.ts
+++ tools/codemod/src/v4.0.0/full.ts
@@ -3,12 +3,14 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { API, FileInfo } from "jscodeshift";
+import abstract from "./abstract";
 import layoutReact from "./layout-react";
 import widgetToConfig from "./widget-to-config";
 
 const transforms = [
   widgetToConfig,
   layoutReact,
+  abstract,
 ];
 
 export default function transformer(file: FileInfo, api: API) {
