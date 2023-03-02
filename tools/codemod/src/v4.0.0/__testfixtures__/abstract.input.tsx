e333e8cf1e9234b550b07c6aee66d4f73d213d8b
GerardasB <10091419+GerardasB@users.noreply.github.com>
2023-03-03 00:45:49 +0200

    changeImports to remove specifiers correctly. (#173)

--- tools/codemod/src/v4.0.0/__testfixtures__/abstract.input.tsx
+++ tools/codemod/src/v4.0.0/__testfixtures__/abstract.input.tsx
@@ -2,4 +2,4 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { BackstageItem, CommonStatusBarItem, isActionItem } from "@itwin/appui-abstract";
+import { BackstageItem, BackstageItemType, CommonStatusBarItem, isActionItem } from "@itwin/appui-abstract";
