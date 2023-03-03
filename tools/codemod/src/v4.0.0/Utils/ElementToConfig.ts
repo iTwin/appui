5b0cedf84a18a13e003e76475e4e5777bb283701
AntanasBuk <124153238+AntanasBuk@users.noreply.github.com>
2023-03-03 11:12:02 +0200

    Extended utility functions

--- tools/codemod/src/v4.0.0/Utils/ElementToConfig.ts
+++ tools/codemod/src/v4.0.0/Utils/ElementToConfig.ts
@@ -2,26 +2,43 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { JSCodeshift, ObjectProperty, JSXAttribute, SpreadProperty, JSXSpreadAttribute } from "jscodeshift";
-import { isAttrOrProp } from "./TypeCheck";
+import { ExpressionKind } from "ast-types/gen/kinds";
+import { IdentifierKind, JSXElementKind, JSXExpressionContainerKind, JSXFragmentKind, JSXIdentifierKind, LiteralKind } from "ast-types/gen/kinds";
+import { JSCodeshift, ObjectProperty, JSXAttribute, SpreadProperty, JSXSpreadAttribute, Identifier, Expression } from "jscodeshift";
+import { isAttrOrProp, isJSXEmptyExpression, isJSXExpressionContainer } from "./TypeCheck";
 
-export interface ConfigProperty extends Omit<ObjectProperty, "type" | "key"> {
+export interface ElementAttribute extends Omit<JSXAttribute, "type" | "name" | "value"> {
+  type: "ElementAttribute";
+  name?: JSXIdentifierKind;
+  value: Exclude<JSXAttribute["value"], null> | JSXSpreadAttribute["argument"];
+}
+
+export interface ConfigProperty extends Omit<ObjectProperty, "type" | "key" | "value"> {
   type: "ConfigProperty";
-  name: ObjectProperty["key"];
+  name?: ObjectProperty["key"];
+  value: ObjectProperty["value"] | SpreadProperty["argument"];
 }
 
-export interface ConfigSpreadProperty extends Omit<SpreadProperty, "type"> {
-  type: "ConfigSpreadProperty";
-}
+type NameType = ElementAttribute["name"] | ConfigProperty["name"];
+type ValueType = ElementAttribute["value"] | ConfigProperty["value"];
 
-export type AttributeHandle = (j: JSCodeshift, attr: JSXAttribute | JSXSpreadAttribute | ConfigProperty | ConfigSpreadProperty) => ConfigProperty | ConfigSpreadProperty;
+export type AttributeHandle = (j: JSCodeshift, attr: ElementAttribute | ConfigProperty) => ConfigProperty;
 
-export function buildProperty(j: JSCodeshift, name: ObjectProperty["key"], value: ObjectProperty["value"]): ConfigProperty {
-  const prop: Omit<ObjectProperty, "type"> = j.objectProperty(name, value);
+export function buildConfigProperty(j: JSCodeshift, value: ValueType, name?: NameType): ConfigProperty {
+  if (isSpreadExpression(name, value)) {
+    const prop: Omit<ObjectProperty, "type"> = j.objectProperty(name, value);
+    return {
+      type: "ConfigProperty",
+      name: prop.key,
+      ...prop,
+    };
+  }
+
+  const spreadProp: Omit<SpreadProperty, "type"> = j.spreadProperty(value);
   return {
     type: "ConfigProperty",
-    name: prop.key,
-    ...prop,
+    value: spreadProp.argument,
+    ...spreadProp,
   };
 }
 
@@ -32,17 +49,37 @@ export function chain(first: AttributeHandle, second: AttributeHandle): Attribut
   };
 }
 
-//const skipAttribute: AttributeHandle = () => [{}, {}];
-
-export function renameAttribute(newName: string): AttributeHandle {
+export function rename(newName: string): AttributeHandle {
   return (j, attr) => {
     if (isAttrOrProp(j, attr)) {
-      return buildProperty(j, j.identifier(newName), attr.value);
+      return buildConfigProperty(j, attr.value, j.identifier(newName));
     }
     throw new Error('Cannot perform rename');
   }
 }
 
-// const unwrapAttribute: AttributeHandle = (j, attr) => { return [{}, {}]; }
+export const identity: AttributeHandle = (j, attr) => {
+  return buildConfigProperty(j, attr.value, attr.name);
+}
 
-export const frontstageAttrHandles = new Map<string, AttributeHandle>();
+export const unwrapExpressionContainer: AttributeHandle = (j, attr) => {
+
+  const expr = isJSXExpressionContainer(j, attr.value) ? attr.value.expression : attr.value;
+  if (isJSXEmptyExpression(j, expr))
+    throw new Error("Attribute cannot contain empty expression");
+  return buildConfigProperty(j, expr, attr.name);
+};
+
+
+export const frontstageAttrHandles = new Map<string, AttributeHandle | undefined>([
+  ["key", undefined],
+
+]);
+
+function getName(j: JSCodeshift, attr: JSXAttribute | ConfigProperty): string {
+  return (j(attr.name).get() as Identifier).name;
+}
+
+function isSpreadExpression(name: NameType | undefined, expr: any): expr is Exclude<JSXAttribute["value"], null> | ObjectProperty["value"] {
+  return name ? true : false;
+}
