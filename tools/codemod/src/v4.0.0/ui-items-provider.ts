71e839a0ec905ef9a9308e0e5c526ecfa7892e90
GerardasB <10091419+GerardasB@users.noreply.github.com>
2023-03-08 18:20:31 +0200

    Add 'ui-items-provider' codemod. (#194)

---
+++ tools/codemod/src/v4.0.0/ui-items-provider.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { API, FileInfo } from "jscodeshift";
+import { isIdentifier } from "../utils/typeGuards";
+
+export default function transformer(file: FileInfo, api: API) {
+  const j = api.jscodeshift;
+
+  const root = j(file.source);
+
+  root.find(j.ClassMethod).forEach((path) => {
+    const key = path.value.key;
+    if (isIdentifier(j, key) && key.name === "provideToolbarButtonItems") {
+      key.name = "provideToolbarItems";
+    }
+  });
+
+  return root.toSource();
+}
