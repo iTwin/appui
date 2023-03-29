8c8a083802a9c6833982914665d5d0e97849dffe
Kayla Conner <124190139+kconner1@users.noreply.github.com>
2023-03-29 08:55:21 -0500

    Removed bug fix lines

--- ui/appui-react/src/appui-react/pickers/ViewSelector.tsx
+++ ui/appui-react/src/appui-react/pickers/ViewSelector.tsx
@@ -348,8 +348,6 @@ export class ViewSelector extends React.Component<ViewSelectorProps, ViewSelecto
    *  Renders ViewSelector component
    */
   public override render() {
-    // if (!this.state.initialized)
-    //   return null;
     const iconSpec = IconSpecUtilities.createWebComponentIconSpec(svgSavedView);
 
     const { imodel, ...props } = this.props; // eslint-disable-line @typescript-eslint/no-unused-vars
