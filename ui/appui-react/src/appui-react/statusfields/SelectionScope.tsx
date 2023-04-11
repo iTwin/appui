949faf844caa8683efc654ce6d325ff9653105c8
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-04-11 18:51:20 +0300

    useRef for consistency with translation strings.

--- ui/appui-react/src/appui-react/statusfields/SelectionScope.tsx
+++ ui/appui-react/src/appui-react/statusfields/SelectionScope.tsx
@@ -33,8 +33,8 @@ interface SelectionScopeFieldProps extends CommonProps {
  * @public
  */
 function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
-  const [label] = React.useState(UiFramework.translate("selectionScopeField.label"));
-  const [toolTip] = React.useState(UiFramework.translate("selectionScopeField.toolTip"));
+  const label = React.useRef(UiFramework.translate("selectionScopeField.label"));
+  const toolTip = React.useRef(UiFramework.translate("selectionScopeField.toolTip"));
 
   const options = React.useMemo(() => props.availableSelectionScopes.map((scope) => {
     return { value: scope.id, label: scope.label };
@@ -53,7 +53,7 @@ function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
       style={props.style}
     >
       <label className="uifw-statusFields-selectionScope-label">
-        {label}:
+        {label.current}:
       </label>
       <Select
         className="uifw-statusFields-selectionScope-selector"
@@ -61,7 +61,7 @@ function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
         options={options}
         onChange={updateSelectValue}
         data-testid="components-selectionScope-selector"
-        title={toolTip}
+        title={toolTip.current}
         size="small" />
     </FooterIndicator >
   );
