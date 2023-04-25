53ee346d27f350e0aa76bd14955e19c323a7baef
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2023-04-25 17:31:21 -0400

    docs: ts5 compatibility

--- common/config/rush/.pnpmfile.cjs
+++ common/config/rush/.pnpmfile.cjs
@@ -45,16 +45,12 @@ const forcedDep = {};
 });
 
 function readPackage(pkg) {
-  if (pkg.name == "typedoc" && pkg.dependencies) {
-    pkg.dependencies["typescript"] = "~4.9.0";
-  }
-
   // Hacky mess: For external packages to this monorepo that have peer dependencies on packages
   // in this repo, we need to do some magic in order to get the peerDeps to point to a correct
   // version of the packages. Update the pkg.json real dependency list to
   // Note that these dependencies are only ever allowed for testing purposes and should not be the
   // dependency of any published packages.
-  else if (pkg.name == "@itwin/map-layers") {
+  if (pkg.name == "@itwin/map-layers") {
     pkg.dependencies["@itwin/appui-layout-react"] = "workspace:*";
     pkg.dependencies["@itwin/appui-react"] = "workspace:*";
     pkg.dependencies["@itwin/components-react"] = "workspace:*";
