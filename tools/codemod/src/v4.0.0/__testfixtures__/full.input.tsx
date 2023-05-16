ab1aa7f9f3299d383a4391483e10cbacc7202906
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-05-16 14:29:37 +0300

    Fix test.

--- tools/codemod/src/v4.0.0/__testfixtures__/full.input.tsx
+++ tools/codemod/src/v4.0.0/__testfixtures__/full.input.tsx
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import { Frontstage } from "@itwin/appui-react";
 import { Dialog } from "@itwin/appui-layout-react";
 import { ToolbarWithOverflow } from "@itwin/components-react";
 
