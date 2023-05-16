c7aa56ddf6438113a217143c45e88be22ec8e636
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-05-16 15:22:34 +0300

    Handle SCSS changes by codemod tool.

--- tools/codemod/src/v4.0.0/__tests__/full.test.ts
+++ tools/codemod/src/v4.0.0/__tests__/full.test.ts
@@ -3,8 +3,53 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { defineTest } from "jscodeshift/src/testUtils";
-import { defaultOptions } from "../../utils/TestUtils";
+import { createDefineInlineCssTest, defaultOptions } from "../../utils/TestUtils";
+import { cssPlugin } from "../full";
+
+const defineInlineCssTest = createDefineInlineCssTest([cssPlugin]);
 
 describe("full", () => {
   defineTest(__dirname, "./full", defaultOptions, "full", { parser: "tsx" });
+
+  describe("CSS", () => {
+    defineInlineCssTest(
+      `.btn { transition-duration: $uicore-speed-fast; }`,
+      `.btn { transition-duration: var(--iui-duration-1); }`,
+      "should replace variable"
+    );
+
+    defineInlineCssTest(
+      `.btn { @include uicore-font-family; }`,
+      `.btn { font-family: var(--iui-font-sans); }`,
+      "should replace mixin"
+    );
+
+    defineInlineCssTest(
+      `
+        /* Test. */
+        .btn {
+          // color: $uicore-xxs;
+          padding: 0 $uicore-xxs;
+          margin: 12px;
+          transition-duration: $uicore-speed-fast;
+          height: $uicore-component-height-large;
+
+          @include uicore-font-family;
+        }
+      `,
+      `
+        /* Test. */
+        .btn {
+          // color: $uicore-xxs;
+          padding: 0 var(--iui-size-3xs);
+          margin: 12px;
+          transition-duration: var(--iui-duration-1);
+          height: 32px;
+
+          font-family: var(--iui-font-sans);
+        }
+      `,
+      "should update multiple values"
+    );
+  });
 });
--- tools/codemod/src/v4.0.0/full.ts
+++ tools/codemod/src/v4.0.0/full.ts
@@ -3,6 +3,8 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import type { API, FileInfo, Options } from "jscodeshift";
+import postcss, { AcceptedPlugin, Plugin } from "postcss";
+import valueParser from "postcss-value-parser";
 import abstract from "./abstract";
 import backstageItem from "./backstage-item";
 import componentsReact from "./components-react";
@@ -39,3 +41,72 @@ export default function transformer(file: FileInfo, api: API, options: Options) 
 
   return src;
 }
+
+function renameValue(from: string, to: string): Plugin {
+  return {
+    postcssPlugin: `rename-value ${from} to ${to}`,
+    Declaration(decl) {
+      const parsed = valueParser(decl.value);
+      parsed.walk((node) => {
+        if (node.value === from)
+          node.value = to;
+        if (node.value === `#{${from}}`)
+          node.value = to;
+      });
+      decl.value = parsed.toString();
+    }
+  };
+}
+
+export const cssPlugin: AcceptedPlugin = postcss([
+  renameValue("$uicore-xxs", "var(--iui-size-3xs)"),
+  renameValue("$uicore-xs", "var(--iui-size-2xs)"),
+  renameValue("$uicore-s", "var(--iui-size-xs)"),
+  renameValue("$uicore-sm", "var(--iui-size-s)"),
+  renameValue("$uicore-m", "var(--iui-size-m)"),
+  renameValue("$uicore-l", "var(--iui-size-l)"),
+  renameValue("$uicore-xl", "var(--iui-size-xl)"),
+  renameValue("$uicore-xxl", "var(--iui-size-2xl)"),
+  renameValue("$uicore-3xl", "var(--iui-size-3xl)"),
+  renameValue("$uicore-speed-fast", "var(--iui-duration-1)"),
+  renameValue("$uicore-speed", "var(--iui-duration-2)"),
+  renameValue("$uicore-speed-slow", "var(--iui-duration-3)"),
+  renameValue("$uicore-sans", "var(--iui-font-sans)"),
+  renameValue("$uicore-monospace", "var(--iui-font-mono)"),
+  renameValue("$uicore-font-family", "var(--iui-font-sans)"),
+  renameValue("$uicore-font-size", "var(--iui-font-size-1)"),
+  renameValue("$uicore-font-size-small", "var(--iui-font-size-0)"),
+  renameValue("$uicore-font-size-leading", "var(--iui-font-size-2)"),
+  renameValue("$uicore-font-size-subheading", "var(--iui-font-size-3)"),
+  renameValue("$uicore-font-size-title", "var(--iui-font-size-4)"),
+  renameValue("$uicore-font-size-headline", "var(--iui-font-size-5)"),
+  renameValue("$uicore-cap-size", "var(--iui-font-size-1)"),
+  renameValue("$uicore-cap-size-small", "var(--iui-font-size-0)"),
+  renameValue("$uicore-cap-size-leading", "var(--iui-font-size-2)"),
+  renameValue("$uicore-cap-size-subheading", "var(--iui-font-size-3)"),
+  renameValue("$uicore-cap-size-title", "var(--iui-font-size-4)"),
+  renameValue("$uicore-cap-size-headline", "var(--iui-font-size-5)"),
+  renameValue("$uicore-font-weight-light", "var(--iui-font-weight-light)"),
+  renameValue("$uicore-font-weight-normal", "var(--iui-font-weight-normal)"),
+  renameValue("$uicore-font-weight-semibold", "var(--iui-font-weight-semibold)"),
+  renameValue("$uicore-font-weight-bold", "var(--iui-font-weight-bold)"),
+  renameValue("$uicore-font-loaded-class", "var(--iui-font-sans)"),
+  renameValue("$uicore-border-radius", "var(--iui-border-radius-1)"),
+  renameValue("$uicore-component-height-small", "24px"),
+  renameValue("$uicore-component-height-normal", "28px"),
+  renameValue("$uicore-component-height-large", "32px"),
+  {
+    postcssPlugin: `replace @mixin uicore-font-family`,
+    AtRule(rule) {
+      if (rule.name !== "include")
+        return;
+      if (rule.params !== "uicore-font-family")
+        return;
+      const declaration = postcss.decl({
+        prop: "font-family",
+        value: "var(--iui-font-sans)",
+      });
+      rule.replaceWith(declaration);
+    }
+  },
+]);
