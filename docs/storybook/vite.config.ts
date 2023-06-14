81346d7aea9ae12ecc441d34966346b5332822ef
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-06-14 13:03:52 +0300

    Fix sb build script.

--- docs/storybook/vite.config.ts
+++ docs/storybook/vite.config.ts
@@ -3,11 +3,30 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { defineConfig } from "vite";
+import viteInspect from "vite-plugin-inspect";
+import {
+  esbuildCommonjs,
+  viteCommonjs,
+  Options,
+} from "@originjs/vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
+
+// Fixes: ReferenceError: require is not defined
+function fixedViteCommonjs(options: Options) {
+  const plugin = viteCommonjs(options);
+  plugin.apply = undefined;
+  return plugin;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    fixedViteCommonjs({
+      include: ["@itwin/core-frontend"],
+    }),
+    viteInspect({ build: true }),
+  ],
   resolve: {
     alias: [
       {
@@ -16,5 +35,10 @@ export default defineConfig({
         replacement: "$1",
       },
     ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(["@itwin/core-frontend"])],
+    },
   },
 });
