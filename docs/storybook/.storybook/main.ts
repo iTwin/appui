368945ee2ddd3fbafd89b07b8f9fe9f62002b134
GerardasB <10091419+GerardasB@users.noreply.github.com>
2023-07-10 16:48:29 +0300

    Add storybook (#369)
    * pnpm create vite storybook --template react-ts
    
    * npx storybook@latest init
    
    * rush update
    
    * Remove vite specific files.
    
    * Add MDX extension.
    
    * Add theme.
    
    * Update intro.
    
    * Add ToolbarComposer story.
    
    * Add useIsBackstageOpen story.
    
    * Remove example stories.
    
    * Fix sb build script.
    
    * Use cpx2
    
    * copy:webfont during build.
    
    * Fix story title.
    
    * Rush update.
    
    * Fix linter issue.
    
    * Add deploy:storybook command.
    
    * Add ContextMenu story.
    
    * Add publish-storybook template.
    
    * Update .prettierignore
    
    * Remove unused SVGs.
    
    * rush update --variant core-3x
    
    * Add missing scripts.
    
    * Remove vite-plugin-inspect
    
    * Bump chunkSizeWarningLimit.
    
    * Disable warnings.
    
    * Separate build:storybook script.
    
    * Use PullRequestNumber instead
    
    * Remove warn disable.
    
    * Update documentation link.
    
    * Use github actions.
    
    * Bump node version.
    
    * Update .github/workflows/publish-storybook.yaml
    
    * Remove duplicate build script.
    
    * Replace deploy:storybook command with GH action.
    
    * Do not minify to avoid mangling component names in code sections.
    
    ---------
    
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>

---
+++ docs/storybook/.storybook/main.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    if (!config.build) config.build = {};
+    config.build.chunkSizeWarningLimit = 5000;
+    // This prevents component name mangling in stories.
+    config.build.minify = false;
+    return config;
+  },
+};
+export default config;
