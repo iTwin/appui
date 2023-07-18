26db11de15699cccdd80c3d5e17999f8c68cc355
anmolshres98 <anmolshres98@users.noreply.github.com>
2023-07-18 18:01:38 -0400

    prettier fix & change file update to ""

--- ui/eslint.config.base.js
+++ ui/eslint.config.base.js
@@ -1,9 +1,9 @@
-/** 
-* Do not import packages directly in this file or it will cause issue 
-* with resolutions (either CI or VSCode)
-* packages like "eslint-config-prettier" and "@itwin/eslint-plugin" MUST be imported within
-* each packages eslint.config.js files. (Unless significant changes occurs in either workflow)
-**/
+/**
+ * Do not import packages directly in this file or it will cause issue
+ * with resolutions (either CI or VSCode)
+ * packages like "eslint-config-prettier" and "@itwin/eslint-plugin" MUST be imported within
+ * each packages eslint.config.js files. (Unless significant changes occurs in either workflow)
+ **/
 module.exports = [
   {
     files: ["**/*.{ts,tsx}"],
