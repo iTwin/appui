628f8443ff1a98bbfe6cf29d627085b47b53ca35
AntanasBuk <124153238+AntanasBuk@users.noreply.github.com>
2023-08-03 13:25:43 +0300

    Add helper tools for working with collections

--- tools/codemod/src/utils/Extensions.ts
+++ tools/codemod/src/utils/Extensions.ts
@@ -2,7 +2,9 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import type { ASTNode, Collection, JSCodeshift } from "jscodeshift";
+import type { ASTNode, ASTPath, Collection, JSCodeshift } from "jscodeshift";
+import type { ASTNode as ASTNodeType, Type } from "ast-types/lib/types";
+import type { NodePath } from "ast-types/lib/node-path";
 import {
   toExpression,
   toExpressionName,
@@ -18,17 +20,113 @@ import retainFirstComment from "./retainFirstComment";
 import { usePlugin } from "./usePlugin";
 
 declare module "jscodeshift/src/collection" {
-  interface Collection<N> extends GlobalMethods {}
+  interface Collection<N> extends GlobalMethods<N> {}
 }
 
-interface GlobalMethods {
+interface GlobalMethods<N> {
+  path(): ASTPath<N>;
+  node(): N;
+  nonEmpty(): this | undefined;
+  single(): this | undefined;
+  navigatePath(...path: (string | number)[]): Collection<any>;
+  findInChildren<T extends ASTNodeType>(
+    type: Type<T>,
+    filter?: RecursiveMatchNode<T>,
+    pathName?: string
+  ): Collection<T>;
+  forEachAsCollection(
+    callback: (path: Collection<N>, i: number, paths: ASTPath<N>[]) => void
+  ): this;
+  filterPath(pathName: string): this;
+  filterType<T extends ASTNodeType>(type: Type<T>): Collection<T>;
+  removeFromParentPath(): this;
   contains(other: Collection): boolean;
   concat(other: Collection): this;
   rename(source: string, target: string): this;
 }
 
 function extensionsPlugin(j: JSCodeshift) {
-  const globalMethods: GlobalMethods = {
+  const globalMethods: GlobalMethods<ASTNode> = {
+    /** Returns the first path in the collection */
+    path(this: Collection) {
+      return this.paths()[0];
+    },
+    /** Returns the first node in this collection */
+    node(this: Collection) {
+      return this.nodes()[0];
+    },
+    nonEmpty(this: Collection) {
+      return this.length > 0 ? this : undefined;
+    },
+    single(this: Collection) {
+      return this.length === 1 ? this : undefined;
+    },
+    navigatePath(this: Collection, ...path) {
+      return j(this.get(...path));
+    },
+    /** An alternative to find that only traverses paths of the immediate children of each path in the collection */
+    findInChildren(this: Collection, type, filter, pathName) {
+      const paths: ASTPath[] = [];
+      const visitorMethodName = "visit" + type;
+
+      this.forEach((parentPath: ASTPath) => {
+        const visitor = {
+          [visitorMethodName]: function (path: ASTPath) {
+            if (path === parentPath) {
+              this.traverse(path);
+              return false;
+            }
+
+            if (!path.parentPath || path.parentPath.node !== parentPath.node)
+              return false;
+
+            if (pathName && !isFromPath(path, pathName)) return false;
+
+            if (!filter || matchNode(path.value, filter)) paths.push(path);
+
+            return false;
+          },
+        };
+        j.types.PathVisitor.visit(parentPath as any, visitor);
+      });
+      return j(paths);
+
+      // Implementation with jscodeshift api only
+      /* return this.map((parentPath) =>
+        j(parentPath)
+          .find(type, filter)
+          .filter((childPath) => {
+            if (childPath.parentPath.node !== parentPath.node) return false;
+            if (nodePathName) {
+              const childNodePathName =
+                typeof childPath.name === "number"
+                  ? childPath.parentPath.name
+                  : childPath.name;
+              return nodePathName === childNodePathName;
+            }
+            return true;
+          })
+          .paths()
+      );*/
+    },
+    forEachAsCollection(this: Collection, callback) {
+      return this.forEach((path, i, paths) => callback(j(path), i, paths));
+    },
+    filterPath(this: Collection, pathName) {
+      return this.filter((path) => isFromPath(path, pathName));
+    },
+    filterType(this: Collection, type) {
+      return this.filter((path) => type.check(path.value));
+    },
+    removeFromParentPath(this: Collection) {
+      return this.forEach((path) => {
+        const value = path.parentPath?.value;
+        if (Array.isArray(value)) {
+          const index = value.indexOf(path.node);
+          index >= 0 && value.splice(index, 1);
+        }
+      });
+    },
     contains(this: Collection, other) {
       const paths = this.paths();
       const otherPaths = other.paths();
@@ -158,6 +256,43 @@ function extensionsPlugin(j: JSCodeshift) {
 
 export function useExtensions(j: JSCodeshift) {
   usePlugin(j, extensionsPlugin);
+}
+
+type RecursiveMatchNode<T> =
+  | (T extends {}
+      ? {
+          [K in keyof T]?: RecursiveMatchNode<T[K]>;
+        }
+      : T)
+  | ((value: T) => boolean);
+
+const hasOwn = Object.prototype.hasOwnProperty.call.bind(
+  Object.prototype.hasOwnProperty
+);
+
+function isNode(value: any): boolean {
+  return typeof value === "object" && value;
+}
+
+function matchNode(haystack: any, needle: any): boolean {
+  if (typeof needle === "function") {
+    return needle(haystack);
+  }
+  if (isNode(needle) && isNode(haystack)) {
+    return Object.keys(needle).every(
+      (property) =>
+        hasOwn(haystack, property) &&
+        matchNode(haystack[property], needle[property])
+    );
+  }
+  return haystack === needle;
+}
+
+function isFromPath(path: ASTPath<any>, pathName: string): boolean {
+  if (typeof path.name === "number")
+    return path.parentPath ? path.parentPath.name === pathName : false;
+
+  return path.name === pathName;
 }
 
 function parseRename(rename: string) {
