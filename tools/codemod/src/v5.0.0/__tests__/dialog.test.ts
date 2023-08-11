c4ba4ab3918b6061f9b5cb026f35b37b2405951a
AntanasBuk <124153238+AntanasBuk@users.noreply.github.com>
2023-08-11 17:45:09 +0300

    Tests on TitleBar

--- tools/codemod/src/v5.0.0/__tests__/dialog.test.ts
+++ tools/codemod/src/v5.0.0/__tests__/dialog.test.ts
@@ -191,18 +191,172 @@ describe("dialog", () => {
     );
   });
 
-  /*describe("", () => {
+  describe("TitleBar", () => {
     defineInlineTest(
       `
       import { Dialog } from "@itwin/core-react";
+      <Dialog title="MyTitle" titleStyle={props.style} />
       `,
       `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          <Dialog.TitleBar titleText="MyTitle" style={props.style} />
+        </Dialog.Main>
+      </Dialog>
       `,
-      "should "
+      "should move attributes to TitleBar"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog hideHeader />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+      </Dialog>
+      `,
+      "should not add TitleBar expression when hideHeader value is null"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog hideHeader={true} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+      </Dialog>
+      `,
+      "should not add TitleBar expression when hideHeader value is true"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog hideHeader={getHideHeaderValue(this)} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          {!getHideHeaderValue(this) && <Dialog.TitleBar />}
+        </Dialog.Main>
+      </Dialog>
+      `,
+      "should add logical expression (&&) with negated hideHeader value and TitleBar"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog hideHeader={false} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          <Dialog.TitleBar />
+        </Dialog.Main>
+      </Dialog>
+      `,
+      "should not add hideHeader in TitleBar expression when hideHeader value is false"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog hideHeader={hideHeaderVal} header={headerVal} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          {!hideHeaderVal && (headerVal || <Dialog.TitleBar />)}
+        </Dialog.Main>
+      </Dialog>
+      `,
+      "should add nested logical expressions with negated hideHeader value, header value and TitleBar"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog header={headerVal} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          {headerVal || <Dialog.TitleBar />}
+        </Dialog.Main>
+      </Dialog>
+      `,
+      "should add logical expression (||) with header value and TitleBar"
+    );
+    defineInlineTest(
+      `
+      import { Dialog } from "@itwin/core-react";
+      <Dialog header={<h>Header</h>} />
+      `,
+      `
+      import { Dialog } from "@itwin/itwinui-react";
+      <Dialog
+        preventDocumentScroll={true}
+        style={{
+          zIndex: "var(--uicore-z-index-dialog)",
+        }}
+        closeOnEsc={false}>
+        <Dialog.Backdrop />
+        <Dialog.Main>
+          <h>Header</h>
+        </Dialog.Main>
+      </Dialog>
+      `,
+      "should add only header as TitleBar expression"
     );
   });
 
-  describe("", () => {
+  /*describe("", () => {
     defineInlineTest(
       `
       import { Dialog } from "@itwin/core-react";
