c9d1a471dfaf6726e7449aa286d8c33eca6c6070
Joe Henry <70602409+joehenry9498@users.noreply.github.com>
2023-08-30 13:23:01 -0400

    Fix tree spacing with loading spinner and imports

--- test-apps/appui-test-app/appui-test-providers/src/ui/widgets/TreeWidget.tsx
+++ test-apps/appui-test-app/appui-test-providers/src/ui/widgets/TreeWidget.tsx
@@ -7,10 +7,9 @@ import {
   ControlledTree,
   ControlledTreeProps,
   ImmediatelyLoadedTreeNodeItem,
-  ITreeDataProvider,
-  PageOptions,
   SelectionMode,
-  TreeNodeItem,
+  SimpleTreeDataProvider,
+  SimpleTreeDataProviderHierarchy,
   useTreeEventsHandler,
   useTreeModel,
   useTreeModelSource,
@@ -94,46 +93,4 @@ export function TreeWidgetComponent() {
       <ControlledTree {...defaultProps} />
     </div>
   );
-}
-
-export type SimpleTreeDataProviderHierarchy = Map<
-  string | undefined,
-  TreeNodeItem[]
->;
-
-class SimpleTreeDataProvider implements ITreeDataProvider {
-  private _hierarchy: SimpleTreeDataProviderHierarchy;
-
-  public constructor(hierarchy: SimpleTreeDataProviderHierarchy) {
-    this._hierarchy = hierarchy;
-  }
-
-  private getNodesByParentId(
-    parentId?: string,
-    pageOptions?: PageOptions
-  ): TreeNodeItem[] {
-    const nodes = this._hierarchy.get(parentId);
-
-    if (!nodes) return [];
-
-    if (!pageOptions) return [...nodes];
-
-    let pageEndIndex: number | undefined;
-    if (pageOptions.size !== undefined && pageOptions.size !== 0) {
-      pageEndIndex = pageOptions.size;
-      pageEndIndex += pageOptions.start ? pageOptions.start : 0;
-    }
-    return nodes.slice(pageOptions.start, pageEndIndex);
-  }
-
-  public async getNodes(
-    parent?: TreeNodeItem,
-    pageOptions?: PageOptions
-  ): Promise<TreeNodeItem[]> {
-    return this.getNodesByParentId(parent ? parent.id : undefined, pageOptions);
-  }
-
-  public async getNodesCount(parent?: TreeNodeItem): Promise<number> {
-    return this.getNodesByParentId(parent ? parent.id : undefined).length;
-  }
 }
