52b9d73a885668e3c04d339b8622e86cfcb12202
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-09-18 10:47:35 +0300

    Use AppUiDecorator.

--- docs/storybook/src/components/ContextMenu.stories.tsx
+++ docs/storybook/src/components/ContextMenu.stories.tsx
@@ -3,15 +3,15 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import type { Meta, StoryObj } from "@storybook/react";
-import { StateManager, ThemeManager } from "@itwin/appui-react";
 import { ContextMenuItem, ContextSubMenu } from "@itwin/core-react";
 import { ContextMenu } from "@itwin/core-react/src/core-react/contextmenu/ContextMenu";
-import { Provider } from "react-redux";
+import { AppUiDecorator } from "../AppUiDecorator";
 
 const meta = {
   title: "Components/ContextMenu",
   component: ContextMenu,
   tags: ["autodocs"],
+  decorators: [AppUiDecorator],
 } satisfies Meta<typeof ContextMenu>;
 
 export default meta;
@@ -22,24 +22,18 @@ export const Basic: Story = {
     opened: true,
   },
   render: (props) => {
-    // TODO:
-    new StateManager();
     return (
-      <Provider store={StateManager.store}>
-        <ThemeManager>
-          <ContextMenu {...props}>
-            <ContextSubMenu label="Test 1" id="1">
-              <ContextMenuItem>Test 1.1</ContextMenuItem>
-            </ContextSubMenu>
-            <ContextSubMenu label="Test 2" id="2">
-              <ContextMenuItem>Test 2.1</ContextMenuItem>
-              <ContextSubMenu label="Test 2.2" id="22">
-                <ContextMenuItem>Test 2.2.1</ContextMenuItem>
-              </ContextSubMenu>
-            </ContextSubMenu>
-          </ContextMenu>
-        </ThemeManager>
-      </Provider>
+      <ContextMenu {...props}>
+        <ContextSubMenu label="Test 1" id="1">
+          <ContextMenuItem>Test 1.1</ContextMenuItem>
+        </ContextSubMenu>
+        <ContextSubMenu label="Test 2" id="2">
+          <ContextMenuItem>Test 2.1</ContextMenuItem>
+          <ContextSubMenu label="Test 2.2" id="22">
+            <ContextMenuItem>Test 2.2.1</ContextMenuItem>
+          </ContextSubMenu>
+        </ContextSubMenu>
+      </ContextMenu>
     );
   },
 };
