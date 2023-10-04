b48065757e0bdd8c3de0fb3deb8fdd62a08e2031
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-10-04 15:09:43 +0300

    Add canPopout story.

---
+++ docs/storybook/src/widget/CanPopout.stories.tsx
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import type { Meta, StoryObj } from "@storybook/react";
+import { CanPopoutStory } from "./CanPopout";
+import { AppUiDecorator } from "../AppUiDecorator";
+import { Page } from "../AppUiStory";
+
+const meta = {
+  title: "Widget/canPopout",
+  component: CanPopoutStory,
+  tags: ["autodocs"],
+  decorators: [AppUiDecorator],
+  parameters: {
+    docs: {
+      page: () => <Page />,
+    },
+  },
+} satisfies Meta<typeof CanPopoutStory>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    canPopout: true,
+  },
+};
