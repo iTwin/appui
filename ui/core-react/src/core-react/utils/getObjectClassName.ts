f0c272172927b39b99efd32778488e658db87738
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2023-11-08 15:07:47 -0500

    add docs annotations

--- ui/core-react/src/core-react/utils/getObjectClassName.ts
+++ ui/core-react/src/core-react/utils/getObjectClassName.ts
@@ -2,6 +2,10 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Utilities
+ */
+
 /**
  * Returns the name of a class from a static or instance object.
  * @internal
