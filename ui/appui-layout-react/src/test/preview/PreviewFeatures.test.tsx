7650706da05135d0741693530c6f184b33ffe8e6
Raphaël LEMIEUX <1904889+raplemie@users.noreply.github.com>
2023-11-14 14:14:50 -0500

    New `enableMaximizedFloatingWidget` preview feature (#581)
    * Preview `enableMaximizedFloatingWidget`.
    Fix `PreviewFeatures` documentation (all beta now)
    Clean `Actions` to reduce `extract-api` switches issues.
    
    * rush change
    
    * Put PreviewFeatures in Utility section for docs
    
    * NextVersion + Change "Minimize" label to "Restore"
    
    * Test docs
    
    * extract api
    
    * One more try to fix docs...
    
    * rextract-api
    
    * Take 42

--- ui/appui-layout-react/src/test/preview/PreviewFeatures.test.tsx
+++ ui/appui-layout-react/src/test/preview/PreviewFeatures.test.tsx
@@ -7,11 +7,11 @@ import {
   PreviewFeaturesProvider,
   usePreviewFeatures,
 } from "../../appui-layout-react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { expect } from "chai";
 
 describe("PreviewFeatures", () => {
-  it("should expose all props as context values", () => {
+  it("should expose all props as context values, plus `previewState` and `previewDispatch`", () => {
     const testContext: { renderedContext: any } = {
       renderedContext: null,
     };
@@ -25,9 +25,62 @@ describe("PreviewFeatures", () => {
       </PreviewFeaturesProvider>
     );
 
-    expect(testContext.renderedContext).to.deep.equal({
+    const { previewState, previewDispatch, ...additionalContext } =
+      testContext.renderedContext;
+
+    expect(additionalContext).to.deep.equal({
       contentAlwaysMaxSize: true,
       randomProp: "randomValue",
     });
+    expect(previewState).to.deep.equal({});
+    expect(previewDispatch).to.be.a("function");
+  });
+  it("should expose `previewDispatch` which updates `previewState`", () => {
+    const testContext: { renderedContext: any } = {
+      renderedContext: null,
+    };
+    function TestComponent() {
+      testContext.renderedContext = usePreviewFeatures();
+      return (
+        <button
+          onClick={() => {
+            testContext.renderedContext.previewDispatch({
+              type: "TEST_ACTION",
+            });
+          }}
+        >
+          Rendered
+        </button>
+      );
+    }
+    render(
+      <PreviewFeaturesProvider contentAlwaysMaxSize randomProp={"randomValue"}>
+        <TestComponent />
+      </PreviewFeaturesProvider>
+    );
+
+    expect(testContext.renderedContext.previewState).to.deep.equal({});
+
+    fireEvent.click(document.querySelector("button")!);
+
+    expect(testContext.renderedContext.previewState).to.deep.equal({
+      tested: true,
+    });
+  });
+  it("usePreviewFeatures().previewDispatch should not crash if provider is not used.", () => {
+    const testContext: { renderedContext: any } = {
+      renderedContext: null,
+    };
+    function TestComponent() {
+      testContext.renderedContext = usePreviewFeatures();
+      return <div>Rendered</div>;
+    }
+    render(<TestComponent />);
+
+    const { previewState, previewDispatch } = testContext.renderedContext;
+
+    expect(previewState).to.deep.equal({});
+    expect(previewDispatch).to.be.a("function");
+    expect(previewDispatch).to.not.throw();
   });
 });
