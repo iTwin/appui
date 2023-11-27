156aa6d35e6b68da89741d7a1ea0bacdf5322472
Raphaël LEMIEUX <1904889+raplemie@users.noreply.github.com>
2023-11-27 04:14:14 -0500

    Added expected usage in documentation for some classes. (#603)

--- ui/appui-react/src/appui-react/safearea/SafeAreaContext.tsx
+++ ui/appui-react/src/appui-react/safearea/SafeAreaContext.tsx
@@ -9,8 +9,15 @@
 import * as React from "react";
 import type { SafeAreaInsets } from "./SafeAreaInsets";
 
-/**
- * Context used to manage safe area (feature used by devices with non-rectangular screens).
+/** Context used to manage safe area (feature used by devices with non-rectangular screens).
+ *
+ * If used, this is expected to wrap [[ConfigurableUiContent]].
+ *
+ * ```tsx
+ *  <SafeAreaContext.Provider value={SafeAreaInsets.All}>
+ *    <ConfigurableUiContent />
+ *  </SafeAreaContext.Provider>
+ * ```
  * @public
  */
 export const SafeAreaContext = React.createContext<SafeAreaInsets | undefined>(
