73a4241a3409b7af58a0f969b70775f400f33cd9
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-12-07 16:49:58 +0200

    Enable newToolbars in standalone test app.

--- test-apps/appui-test-app/appui-test-providers/src/ui/providers/PreviewFeaturesToggleProvider.tsx
+++ test-apps/appui-test-app/appui-test-providers/src/ui/providers/PreviewFeaturesToggleProvider.tsx
@@ -33,6 +33,7 @@ function PreviewFeatureList() {
       enableMaximizedFloatingWidget: activeFeatureList.includes(
         "enableMaximizedFloatingWidget"
       ),
+      newToolbars: true,
     });
   }, [activeFeatureList]);
 
