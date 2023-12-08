3dfaa84c562255fc23b978844add1217adbb1c4f
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-12-08 13:29:04 +0200

    Fix tests.

--- full-stack-tests/ui/tests/popout-widget.test.ts
+++ full-stack-tests/ui/tests/popout-widget.test.ts
@@ -66,7 +66,6 @@ test.describe("popout widget", () => {
     const widget = widgetLocator({ tab });
     const popoutButton = popoutButtonLocator(widget);
 
-    // Popout the widget w/ default size.
     let [popoutPage] = await Promise.all([
       context.waitForEvent("page"),
       popoutButton.click(),
@@ -91,7 +90,6 @@ test.describe("popout widget", () => {
     const widget = widgetLocator({ tab });
     const popoutButton = popoutButtonLocator(widget);
 
-    // Popout the widget w/ default size.
     let [popoutPage] = await Promise.all([
       context.waitForEvent("page"),
       popoutButton.click(),
@@ -136,7 +134,6 @@ test.describe("popout widget", () => {
     await tab.click();
     await expect(tab).toHaveClass(/nz-active/);
 
-    // Popout the widget.
     [popoutPage] = await Promise.all([
       context.waitForEvent("page"),
       popoutButton.click(),
@@ -155,7 +152,6 @@ test.describe("popout widget", () => {
     const widget = widgetLocator({ tab });
     const popoutButton = popoutButtonLocator(widget);
 
-    // Popout the widget w/ default size.
     let [popoutPage] = await Promise.all([
       context.waitForEvent("page"),
       popoutButton.click(),
@@ -169,20 +165,15 @@ test.describe("popout widget", () => {
     });
     await popoutPage.close({ runBeforeUnload: true });
 
-    // TODO: ATM need to activate the tab, since the widget is not floating after window is closed
-    await tab.click();
-    await expect(tab).toHaveClass(/nz-active/);
-
     await expectSavedFrontstageState(context, (state) => {
       return (
-        state.nineZone.widgets.leftStart?.activeTabId ===
-        "appui-test-providers:ViewAttributesWidget"
+        state.nineZone.widgets["appui-test-providers:ViewAttributesWidget"]
+          ?.activeTabId === "appui-test-providers:ViewAttributesWidget"
       );
     });
 
     await page.reload();
 
-    // Popout the widget.
     [popoutPage] = await Promise.all([
       context.waitForEvent("page"),
       popoutButton.click(),
@@ -217,7 +208,8 @@ test.describe("popout widget", () => {
     );
     await expect.poll(async () => popoutPage.isClosed()).toBe(true);
   });
-  test("should render popout, mount content to WidgetContainer, and then set widget to floating", async ({
+
+  test("should not unmount when widget is popped out", async ({
     context,
     page,
   }) => {
@@ -233,15 +225,10 @@ test.describe("popout widget", () => {
       context.waitForEvent("page"),
       popoutButton.click(),
     ]);
-    await popoutPage.waitForLoadState(); // TODO: childWindow is only added after 'load' event
     expect(popoutPage.isClosed()).toBe(false);
 
-    popoutPage.close();
+    await popoutPage.close();
 
-    await setWidgetState(page, id, WidgetState.Floating);
-    await expect.poll(async () => popoutPage.isClosed()).toBe(true);
-    // Due to the change to Popouts where `WidgetContentContainer` is displayed instead of a React Node. The actual widget is no longer unmounted
-    // and is simply sent to a different `WidgetContentContainer`. That's why the unmount count was changed to 0 here.
     expect(widgetLifecycle.mountCount).toBe(1);
     expect(widgetLifecycle.unMountCount).toBe(0);
   });
