9d804a7be6104ec4ddbc249f2d30884220074095
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2024-01-10 23:24:48 +0200

    Add global initializer.

--- ui/appui-react/src/test/setup.test.ts
+++ ui/appui-react/src/test/setup.test.ts
@@ -2,8 +2,21 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import { IModelApp, NoRenderApp } from "@itwin/core-frontend";
+import TestUtils from "./TestUtils";
+
 afterEach(() => {
   // Undo DOM manipulations made by iTwinUI-React components
   document.body.innerHTML = "";
   document.body.removeAttribute("class");
+});
+
+beforeEach(async () => {
+  await NoRenderApp.startup();
+  await TestUtils.initializeUiFramework();
+});
+
+afterEach(async () => {
+  TestUtils.terminateUiFramework();
+  await IModelApp.shutdown();
 });
