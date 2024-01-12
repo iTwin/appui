e9f8bfbf877d5909396803b6eb8378edb494e30c
Morgan Adrales <85196633+morganadrales@users.noreply.github.com>
2024-01-12 10:45:02 -0600

    Fix lint error

--- ui/components-react/src/components-react/filter-builder/FilterBuilderRuleOperator.tsx
+++ ui/components-react/src/components-react/filter-builder/FilterBuilderRuleOperator.tsx
@@ -66,7 +66,7 @@ export function PropertyFilterBuilderRuleOperator(
         options={availableOptions}
         value={selectedOperator}
         onChange={onChange}
-        size={getiTwinUISize(props.size)}
+        size={getiTwinUISize(size)}
       />
     </div>
   );
