7d330a5855e79eafd8445e795b9f2ceea816cb3a
KaroAug <143687681+KaroAug@users.noreply.github.com>
2024-01-19 15:34:50 +0200

    Run codemod tests in CI pipeline

--- tools/codemod/jest.config.js
+++ tools/codemod/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   transform: { "\\.ts$": ["ts-jest"] },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test",
+        outputName: "codemod_test_results.xml",
+      },
+    ],
+  ],
 };
