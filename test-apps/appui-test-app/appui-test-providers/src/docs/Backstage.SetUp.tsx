c8888eebb931eb768e7dd3e9c5d50d65b9027f45
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-01-22 11:40:53 +0200

    Add code snippets for the documentation (#615)
    * Code snippets for frontstage page.
    
    * Add docs script.
    
    * Add UiItemsProvider snippets.
    
    * Update snippets.
    
    * Move snippets to test-providers.
    
    * Add ToolSettings.SupplyProperties snippet.
    
    * Add Backstage.SetUp snippet.
    
    * Fix linter error.
    
    * Fix linter errors.
    
    * Fix backstage tests.
    
    * Remove webfont usage from code snippets.
    
    * Add a code snippet for conditional.
    
    * rush update
    
    * Fix https://github.com/advisories/GHSA-c24v-8rfc-w8vw
    
    * Keep imodeljs-darwin 3.6.9 fix.

---
+++ test-apps/appui-test-app/appui-test-providers/src/docs/Backstage.SetUp.tsx
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+// __PUBLISH_EXTRACT_START__ AppUI.Backstage.SetUp
+import { BackstageComposer, ConfigurableUiContent } from "@itwin/appui-react";
+
+<ConfigurableUiContent appBackstage={<BackstageComposer />} />;
+// __PUBLISH_EXTRACT_END__
