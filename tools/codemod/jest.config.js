2db1bdcc910e8b8f87e23f98ec3723c4d40877a8
KaroAug <143687681+KaroAug@users.noreply.github.com>
2024-01-29 14:02:02 +0200

    edit configurations

--- tools/codemod/jest.config.js
+++ tools/codemod/jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   transform: { "\\.ts$": ["ts-jest"] },
-  reporters: [
-    "default",
-    [
-      "jest-junit",
-      {
-        outputDirectory: "test",
-        outputName: "codemod_test_results.xml",
-      },
-    ],
-  ],
+  reporters: ["default", ["jest-junit", {}]],
 };
