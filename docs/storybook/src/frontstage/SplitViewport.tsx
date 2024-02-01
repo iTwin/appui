336d788d598c8a545c2e617b2c60535bcea0db35
Joe Henry <70602409+joehenry9498@users.noreply.github.com>
2024-02-01 04:20:47 -0500

    Add story for split viewport (#704)
    * Add story for split viewport
    
    * Added isHidden and isDisabled to storybook

---
+++ docs/storybook/src/frontstage/SplitViewport.tsx
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { AppUiStory, AppUiStoryProps } from "../AppUiStory";
+
+/** This story shows two separate views. Depending on which view is active, the tools in the toolbars will change*/
+export function SplitViewportStory(
+  props: Pick<AppUiStoryProps, "frontstageProviders" | "itemProviders">
+) {
+  return <AppUiStory layout="fullscreen" {...props} />;
+}
