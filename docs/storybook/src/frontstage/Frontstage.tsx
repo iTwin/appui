b2015e0845e054795253e80ed0e797d429d62c3e
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2024-02-09 19:01:50 +0200

    Add capability to hide/show tool settings and status bar in Frontstage stories.

--- docs/storybook/src/frontstage/Frontstage.tsx
+++ docs/storybook/src/frontstage/Frontstage.tsx
@@ -2,11 +2,27 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import { StandardFrontstageProps } from "@itwin/appui-react";
 import { AppUiStory, AppUiStoryProps } from "../AppUiStory";
+import { createFrontstageProvider } from "../Utils";
+
+type FrontstageStoryProps = Pick<AppUiStoryProps, "itemProviders"> &
+  Pick<StandardFrontstageProps, "hideStatusBar" | "hideToolSettings"> & {
+    frontstage?: Partial<StandardFrontstageProps>;
+  };
 
 /** [FrontstageProvider](https://www.itwinjs.org/reference/appui-react/frontstage/frontstageprovider/) can be used to configure a frontstage. */
-export function FrontstageStory(
-  props: Pick<AppUiStoryProps, "frontstageProviders" | "itemProviders">
-) {
-  return <AppUiStory layout="fullscreen" {...props} />;
+export function FrontstageStory(props: FrontstageStoryProps) {
+  const frontstageProvider = createFrontstageProvider({
+    ...props.frontstage,
+    hideStatusBar: props.hideStatusBar,
+    hideToolSettings: props.hideToolSettings,
+  });
+  return (
+    <AppUiStory
+      layout="fullscreen"
+      frontstageProviders={[frontstageProvider]}
+      {...props}
+    />
+  );
 }
