9f0b0146ca1567e2a2db599390996c0532ea8277
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-02-22 12:43:14 +0200

    Add Timeline story.

---
+++ docs/storybook/src/components/Timeline.stories.tsx
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { TimelineComponent } from "@itwin/imodel-components-react/src/imodel-components-react/timeline/TimelineComponent";
+import { action } from "@storybook/addon-actions";
+import type { Decorator, Meta, StoryObj } from "@storybook/react";
+import { AppUiDecorator, InitializerDecorator } from "../Decorators";
+
+const AlignComponent: Decorator = (Story) => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "end",
+        paddingInline: "10%",
+        paddingBlock: "2em",
+        boxSizing: "border-box",
+      }}
+    >
+      <Story />
+    </div>
+  );
+};
+
+const PageLayout: Decorator = (Story) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Story />
+    </div>
+  );
+};
+
+const meta = {
+  title: "Components/TimelineComponent",
+  component: TimelineComponent,
+  tags: ["autodocs"],
+  decorators: [
+    AlignComponent,
+    InitializerDecorator,
+    AppUiDecorator,
+    PageLayout,
+  ],
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof TimelineComponent>;
+
+export default meta;
+type Story = StoryObj<typeof TimelineComponent>;
+
+export const Basic: Story = {
+  args: {
+    initialDuration: 5000,
+    totalDuration: 5000,
+    repeat: true,
+    onChange: action("onChange"),
+  },
+};
