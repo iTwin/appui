9e5feb6b359d1c4397d52bb5612a0e6dc5b87d32
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-03-05 19:55:48 +0200

    Replace color picker

--- ui/imodel-components-react/src/imodel-components-react/timeline/SolarTimeline.tsx
+++ ui/imodel-components-react/src/imodel-components-react/timeline/SolarTimeline.tsx
@@ -11,6 +11,10 @@ import classnames from "classnames";
 import * as React from "react";
 import {
   Button,
+  ColorBuilder,
+  ColorInputPanel,
+  ColorPalette,
+  ColorPicker,
   DatePicker,
   Flex,
   Icon,
@@ -18,6 +22,7 @@ import {
   Label,
   Popover,
   Slider,
+  Text,
   Tooltip,
   VisuallyHidden,
 } from "@itwin/itwinui-react";
@@ -45,6 +50,8 @@ import { PlayButton } from "./PlayButton";
 const msPerMinute = 1000 * 60;
 const msPerHour = msPerMinute * 60;
 const defaultPlaybackDuration = 40 * 1000;
+
+const offset = 6;
 
 interface TimelineProps extends CommonProps {
   dayStartMs: number;
@@ -462,25 +469,6 @@ export class SolarTimeline extends React.PureComponent<
     return `${hoursStr}:${minutesStr} ${abbrev}`;
   };
 
-  private _onPresetColorPick = (shadowColor: ColorDef) => {
-    this.setState(
-      { shadowColor },
-      () => (this.props.dataProvider.shadowColor = shadowColor)
-    );
-  };
-
-  private _handleHueOrSaturationChange = (hueOrSaturation: HSVColor) => {
-    if (hueOrSaturation.s === 0)
-      // for a ColorDef to be created from hsv s can't be 0
-      hueOrSaturation = hueOrSaturation.clone(undefined, 0.5);
-
-    const shadowColor = hueOrSaturation.toColorDef();
-    this.setState(
-      { shadowColor },
-      () => (this.props.dataProvider.shadowColor = shadowColor)
-    );
-  };
-
   public getLocalTime(ticks: number): Date {
     const projectTime = new Date(ticks);
     // convert project date to browser locale date
@@ -515,7 +503,7 @@ export class SolarTimeline extends React.PureComponent<
     };
 
     return (
-      <div className={"solar-timeline-wrapper"}>
+      <div className="solar-timeline-wrapper">
         <Flex flexWrap="wrap" gap="none" className="solar-timeline-start">
           <PlayButton
             isPlaying={this.state.isPlaying}
@@ -537,7 +525,7 @@ export class SolarTimeline extends React.PureComponent<
             }}
             placement="top"
             middleware={{
-              offset: 6,
+              offset,
             }}
           >
             <CalendarButton
@@ -594,96 +582,43 @@ export class SolarTimeline extends React.PureComponent<
             <SvgLoop />
           </IconButton>
 
-          <IconButton
-            styleType="borderless"
-            data-testid="shadow-settings-button"
-            label={this._settingLabel}
-            ref={(element) => (this._settings = element)}
-            onClick={this._onOpenSettingsPopup}
+          <Popover
+            content={
+              <ColorPicker
+                selectedColor={this.state.shadowColor.colors}
+                onChangeComplete={(color) => {
+                  const colorDef = ColorDef.create(color.toTbgr());
+                  this.setState(
+                    { shadowColor: colorDef },
+                    () => (this.props.dataProvider.shadowColor = colorDef)
+                  );
+                }}
+              >
+                <Text variant="title" as="h2" style={{ textAlign: "center" }}>
+                  {this._settingsPopupTitle}
+                </Text>
+                <ColorBuilder />
+                <ColorInputPanel defaultColorFormat="hsl" />
+                <ColorPalette
+                  colors={this._presetColors.map((color) => color.colors)}
+                />
+              </ColorPicker>
+            }
+            placement="top"
+            middleware={{
+              offset,
+            }}
           >
-            <SvgSettings />
-          </IconButton>
-
-          <Popup
-            className="shadow-settings-popup"
-            target={this._settings}
-            offset={11}
-            isOpen={this.state.isSettingsOpened}
-            onClose={this._onCloseSettingsPopup}
-            position={RelativePosition.Top}
-          >
-            <div className="shadow-settings-popup-container">
-              <div className="shadow-settings-header">
-                {this._settingsPopupTitle}
-              </div>
-              <div className="shadow-settings-color">
-                <div className="shadow-settings-color-top">
-                  <SaturationPicker
-                    hsv={this.state.shadowColor.toHSV()}
-                    onSaturationChange={this._handleHueOrSaturationChange}
-                  />
-                </div>
-                <div className="shadow-settings-color-bottom">
-                  <div className="shadow-settings-color-bottom-left">
-                    <HueSlider
-                      hsv={this.state.shadowColor.toHSV()}
-                      onHueChange={this._handleHueOrSaturationChange}
-                      isHorizontal={true}
-                    />
-                  </div>
-                  <div className="shadow-settings-color-bottom-right">
-                    <ColorSwatch
-                      style={colorSwatchStyle}
-                      colorDef={this.state.shadowColor}
-                      round={false}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="shadow-settings-color-presets">
-                <ColorSwatch
-                  colorDef={this._presetColors[0]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[1]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[2]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[3]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[4]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[5]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[6]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-                <ColorSwatch
-                  colorDef={this._presetColors[7]}
-                  round={false}
-                  onColorPick={this._onPresetColorPick}
-                />
-              </div>
-            </div>
-          </Popup>
+            <IconButton
+              styleType="borderless"
+              data-testid="shadow-settings-button"
+              label={this._settingLabel}
+              ref={(element) => (this._settings = element)}
+              onClick={this._onOpenSettingsPopup}
+            >
+              <SvgSettings />
+            </IconButton>
+          </Popover>
         </Flex>
       </div>
     );
