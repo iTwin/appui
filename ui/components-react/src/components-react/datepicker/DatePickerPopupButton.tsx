1e244aa37edbf75f1dad8404c28cd7399fab6879
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-03-05 21:02:46 +0200

    Deprecate DatePickerPopupButton.

--- ui/components-react/src/components-react/datepicker/DatePickerPopupButton.tsx
+++ ui/components-react/src/components-react/datepicker/DatePickerPopupButton.tsx
@@ -20,7 +20,9 @@ import { TimeField } from "./TimeField";
 import "./DatePickerPopupButton.scss";
 
 /** Props used by [[DatePickerPopupButton]] component.
- * @alpha */
+ * @alpha
+ * @deprecated in 4.11.x. Props of deprecated component {@link DatePickerPopupButton}.
+ */
 export interface DatePickerPopupButtonProps extends CommonProps {
   /** Date to be shown as the selected date. */
   selected: Date;
@@ -43,7 +45,8 @@ export interface DatePickerPopupButtonProps extends CommonProps {
 
 /** Component that displays a button used to pick a date and optionally a time.
  * @alpha
- * */
+ * @deprecated in 4.11.x. Use {@link https://itwinui.bentley.com/docs/datepicker iTwinUI date picker} instead.
+ */
 export function DatePickerPopupButton({
   displayEditField,
   timeDisplay,
