24bf482be28a55b433605e1cd40e24e432a28638
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-03-07 06:38:53 -0500

    Solar timeline enhancements (#733)
    * Replace custom UI components with iTwinUI.
    
    * Fix build issue.
    
    * Add sb story.
    
    * Add timeline component story.
    
    * Remove unused backrounds
    
    * Use `display: grid` instead.
    
    * Cleaner CSS.
    
    * Renamed tooltips & titles.
    
    * Container queries and better spacing.
    
    * Add visually hidden label for speed `<select>`.
    
    * Adding labels for screen readers.
    
    * Use flex component
    
    * Update lock file
    
    * Merge quirk
    
    * Convert PlayButton to function component.
    
    * Controlled PlayButton
    
    * Use iTwinUI components in PlayButton.
    
    * Use iTwinUI date picker.
    
    * Replace TimeField with iTwinUI
    
    * Replace Popup /w Popover
    
    * Render hours and minutes w/ 2 digits.
    
    * Remove duplicate story
    
    * Fix sb
    
    * Render tooltips.
    
    * Istanbul ignores
    
    * Replace color picker
    
    * Remove unused state.
    
    * Fix solar shadow color.
    
    * Fix speed.
    
    * rush change
    
    * NextVersion
    
    * Fix tests.
    
    * Use value attribute in select.
    
    * Lower coverage
    
    * Fix linter error.
    
    * Coverage
    
    * Use label for timeline settings button.
    
    * Update TimelineComponent snap.
    
    ---------
    
    Co-authored-by: Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>

--- docs/storybook/src/components/SolarTimeline.stories.tsx
+++ docs/storybook/src/components/SolarTimeline.stories.tsx
@@ -2,17 +2,49 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Decorator, Meta, StoryObj } from "@storybook/react";
 import { AppUiDecorator, InitializerDecorator } from "../Decorators";
 import { SolarTimeline } from "@itwin/imodel-components-react/src/imodel-components-react/timeline/SolarTimeline";
 import { BaseSolarDataProvider } from "@itwin/imodel-components-react";
+
+const AlignComponent: Decorator = (Story) => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "end",
+        paddingInline: "10%",
+        paddingBlock: "2em",
+        boxSizing: "border-box",
+      }}
+    >
+      <Story />
+    </div>
+  );
+};
+
+const PageLayout: Decorator = (Story) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Story />
+    </div>
+  );
+};
 
 const meta = {
   title: "Components/SolarTimeline",
   component: SolarTimeline,
   tags: ["autodocs"],
-  decorators: [InitializerDecorator, AppUiDecorator],
+  decorators: [
+    AlignComponent,
+    InitializerDecorator,
+    AppUiDecorator,
+    PageLayout,
+  ],
   parameters: {
+    layout: "fullscreen",
     backgrounds: { default: "background-backdrop" },
   },
 } satisfies Meta<typeof SolarTimeline>;
