e494fe8e028768d2c6cb5bbe4c5725f35058503d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-03-25 18:55:46 +0200

    Install vitest to core-react

---
+++ ui/core-react/vitest.config.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
