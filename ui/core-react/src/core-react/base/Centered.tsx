b00bfb562a2df8568dea4d575fe2e3441470ea07
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-04-05 15:39:29 +0300

    Deprecate FillCentered

--- ui/core-react/src/core-react/base/Centered.tsx
+++ ui/core-react/src/core-react/base/Centered.tsx
@@ -13,7 +13,7 @@ import { Div } from "./Div";
 /** Centered React functional component.
  * Displays content centered vertically and horizontally.
  * @public
- * @deprecated in 4.12.x. Use CSS or {@link https://itwinui.bentley.com/docs/flex iTwinUI Flex} instead.
+ * @deprecated in 4.12.x. Use CSS to style an element or {@link https://itwinui.bentley.com/docs/flex iTwinUI Flex} instead.
  */
 export function Centered(props: CommonDivProps) {
   return <Div {...props} mainClassName="uicore-centered" />;
