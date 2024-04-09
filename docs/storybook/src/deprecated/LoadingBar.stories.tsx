314ea83c0b8039bf6d616742dd375f0379d5c1af
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-04-09 10:08:01 +0300

    Deprecate LoadingBar

---
+++ docs/storybook/src/deprecated/LoadingBar.stories.tsx
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import type { Meta, StoryObj } from "@storybook/react";
+import { LoadingBar } from "@itwin/core-react/src/core-react/loading/LoadingBar";
+import { AppUiDecorator } from "../Decorators";
+
+const meta = {
+  title: "Deprecated/LoadingBar",
+  component: LoadingBar,
+  tags: ["autodocs"],
+  decorators: [AppUiDecorator],
+} satisfies Meta<typeof LoadingBar>;
+
+export default meta;
+type Story = StoryObj<typeof LoadingBar>;
+
+export const Basic: Story = {};
