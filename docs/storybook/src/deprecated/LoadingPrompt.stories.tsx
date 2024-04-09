a997288c62f8566c519a2747791d7a099a089cad
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-04-09 10:11:46 +0300

    Deprecate LoadingPrompt

---
+++ docs/storybook/src/deprecated/LoadingPrompt.stories.tsx
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import type { Meta, StoryObj } from "@storybook/react";
+import { LoadingPrompt } from "@itwin/core-react/src/core-react/loading/LoadingPrompt";
+import { AppUiDecorator } from "../Decorators";
+
+const meta = {
+  title: "Deprecated/LoadingPrompt",
+  component: LoadingPrompt,
+  tags: ["autodocs"],
+  decorators: [AppUiDecorator],
+} satisfies Meta<typeof LoadingPrompt>;
+
+export default meta;
+type Story = StoryObj<typeof LoadingPrompt>;
+
+export const Basic: Story = {};
