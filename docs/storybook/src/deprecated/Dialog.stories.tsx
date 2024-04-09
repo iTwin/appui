108883b034bcd7be30d5f30b11f06d123b728d5e
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-04-09 10:25:17 +0300

    Add Dialog story.

---
+++ docs/storybook/src/deprecated/Dialog.stories.tsx
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dialog } from "@itwin/core-react/src/core-react/dialog/Dialog";
+import { AppUiDecorator } from "../Decorators";
+import { DialogButtonType } from "@itwin/appui-abstract";
+
+const meta = {
+  title: "Deprecated/Dialog",
+  component: Dialog,
+  tags: ["autodocs"],
+  decorators: [AppUiDecorator],
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+type Story = StoryObj<typeof Dialog>;
+
+export const Basic: Story = {
+  args: {
+    opened: true,
+    title: "Title",
+    children: "Content",
+    buttonCluster: [
+      { type: DialogButtonType.Cancel, onClick: () => {}, label: "Cancel" },
+      { type: DialogButtonType.OK, onClick: () => {}, label: "OK" },
+    ],
+  },
+};
