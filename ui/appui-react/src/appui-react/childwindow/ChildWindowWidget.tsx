0459d685d24577c3adfe8eb567f979110f6ebd6e
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-06 14:47:05 +0300

    Move under preview dir.

--- ui/appui-react/src/appui-react/childwindow/ChildWindowWidget.tsx
+++ ui/appui-react/src/appui-react/childwindow/ChildWindowWidget.tsx
@@ -9,9 +9,8 @@
 import "./ChildWindowWidget.scss";
 import * as React from "react";
 import type { WidgetDef } from "../widgets/WidgetDef";
-import { useRefs } from "@itwin/core-react";
-import { usePopoutsStore } from "../preview/reparent-popout-widgets/usePopoutsStore";
 import { useReparentPopoutWidget } from "../preview/reparent-popout-widgets/useReparentPopoutWidget";
+import { ReparentedPopoutWidget } from "../preview/reparent-popout-widgets/ReparentedPopoutWidget";
 
 interface ChildWindowWidgetProps {
   widgetContainerId: string;
@@ -27,7 +26,7 @@ export function ChildWindowWidget({
 }: ChildWindowWidgetProps) {
   const reparent = useReparentPopoutWidget(widgetDef.id);
   if (reparent) {
-    return <Reparented widgetContainerId={widgetContainerId} />;
+    return <ReparentedPopoutWidget widgetId={widgetContainerId} />;
   }
   return (
     <div
@@ -37,46 +36,4 @@ export function ChildWindowWidget({
       {widgetDef.reactNode}
     </div>
   );
-}
-
-function Reparented({
-  widgetContainerId,
-}: Pick<ChildWindowWidgetProps, "widgetContainerId">) {
-  const setPopout = usePopoutsStore((state) => state.setPopout);
-  const setPopoutRef = React.useCallback<React.RefCallback<HTMLDivElement>>(
-    (instance) => {
-      setPopout(widgetContainerId, instance ?? undefined);
-    },
-    [setPopout, widgetContainerId]
-  );
-  const autoCloseRef = useAutoClosePopovers(widgetContainerId);
-  const ref = useRefs(setPopoutRef, autoCloseRef);
-
-  return (
-    <div
-      className="uifw-childWindow-childWindowWidget_reparented"
-      data-widget-id={widgetContainerId}
-      ref={ref}
-    />
-  );
-}
-
-/** A workaround to close floating/popover elements when a popout widget is closed.
- * There are some issues with the popover alignment, especially if the tab is not active (popover target is not rendered) after closing the popout.
- */
-function useAutoClosePopovers(widgetContainerId: string) {
-  const ref = React.useRef<HTMLDivElement | null>(null);
-  const onClosePopoutWidget = usePopoutsStore(
-    (state) => state.onClosePopoutWidget
-  );
-  React.useEffect(() => {
-    const element = ref.current;
-    if (!element) return;
-    return onClosePopoutWidget.addListener(({ windowId }) => {
-      if (windowId !== widgetContainerId) return;
-      element.dispatchEvent(new MouseEvent("mousedown"));
-      element.dispatchEvent(new PointerEvent("pointerdown"));
-    });
-  }, [widgetContainerId, onClosePopoutWidget]);
-  return ref;
 }
