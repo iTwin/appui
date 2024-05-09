3c8cfebad0f4ff8046c729d0150934ce092d9ffd
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-09 10:17:05 +0300

    Add WithIcons story.

--- docs/storybook/src/components/ContextMenu.stories.tsx
+++ docs/storybook/src/components/ContextMenu.stories.tsx
@@ -6,21 +6,22 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ContextMenuItem, ContextSubMenu } from "@itwin/core-react";
 import { ContextMenu } from "@itwin/core-react/src/core-react/contextmenu/ContextMenu";
 import { AppUiDecorator } from "../Decorators";
+import { SvgPlaceholder } from "@itwin/itwinui-icons-react";
 
 const meta = {
   title: "Components/ContextMenu",
   component: ContextMenu,
   tags: ["autodocs"],
   decorators: [AppUiDecorator],
+  args: {
+    opened: true,
+  },
 } satisfies Meta<typeof ContextMenu>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  args: {
-    opened: true,
-  },
   render: (props) => {
     return (
       <ContextMenu {...props}>
@@ -31,6 +32,26 @@ export const Basic: Story = {
           <ContextMenuItem>Test 2.1</ContextMenuItem>
           <ContextSubMenu label="Test 2.2" id="22">
             <ContextMenuItem>Test 2.2.1</ContextMenuItem>
+          </ContextSubMenu>
+        </ContextSubMenu>
+      </ContextMenu>
+    );
+  },
+};
+
+export const WithIcons: Story = {
+  render: (props) => {
+    return (
+      <ContextMenu {...props}>
+        <ContextSubMenu icon={<SvgPlaceholder />} label="Test 1" id="1">
+          <ContextMenuItem icon={<SvgPlaceholder />}>Test 1.1</ContextMenuItem>
+        </ContextSubMenu>
+        <ContextSubMenu icon={<SvgPlaceholder />} label="Test 2" id="2">
+          <ContextMenuItem icon={<SvgPlaceholder />}>Test 2.1</ContextMenuItem>
+          <ContextSubMenu icon={<SvgPlaceholder />} label="Test 2.2" id="22">
+            <ContextMenuItem icon={<SvgPlaceholder />}>
+              Test 2.2.1
+            </ContextMenuItem>
           </ContextSubMenu>
         </ContextSubMenu>
       </ContextMenu>
