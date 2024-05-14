ec21508aaf5d48741b89b0cf55ae4bfd17cb9792
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-14 18:47:35 +0300

    Update SelectionInfoField.

--- ui/appui-react/src/appui-react/statusfields/SelectionInfo.tsx
+++ ui/appui-react/src/appui-react/statusfields/SelectionInfo.tsx
@@ -9,30 +9,23 @@
 import * as React from "react";
 import type { CommonProps } from "@itwin/core-react";
 import { SelectionCountField } from "./SelectionCount";
-import { useSelector } from "react-redux";
-import { UiFramework } from "../UiFramework";
-import type { FrameworkState } from "../redux/FrameworkState";
+import { useFrameworkState } from "../redux/useFrameworkState";
 
-/**
- * SelectionInfo Status Field React component. This component is designed to be specified in a status bar definition.
+/** `SelectionInfoField` component is designed to be specified in a status bar.
  * It is used to display the number of items in a selection set.
- * This React component is Redux connected.
  * @note Use [[SelectionCountField]] to display custom selection count.
  * @public
  */
 export function SelectionInfoField(props: CommonProps) {
-  const count = useSelector((state: any) => {
-    const frameworkState: FrameworkState | undefined =
-      state[UiFramework.frameworkStateKey];
-    if (!frameworkState) return 0;
-
-    return frameworkState.sessionState.numItemsSelected;
-  });
+  const frameworkState = useFrameworkState();
+  const numItemsSelected = frameworkState
+    ? frameworkState.sessionState.numItemsSelected
+    : 0;
   return (
     <SelectionCountField
       className={props.className}
       style={props.style}
-      count={count}
+      count={numItemsSelected}
     />
   );
 }
