27b286b3681be170fce77e42ad33763037aa292b
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-14 18:43:54 +0300

    Update SelectionScope

--- ui/appui-react/src/appui-react/statusfields/SelectionScope.tsx
+++ ui/appui-react/src/appui-react/statusfields/SelectionScope.tsx
@@ -6,45 +6,39 @@
  * @module StatusBar
  */
 
-import type { CommonProps } from "@itwin/core-react";
-import { Label, Select } from "@itwin/itwinui-react";
+import "./SelectionScope.scss";
 import classnames from "classnames";
 import * as React from "react";
-import type { ConnectedComponent } from "react-redux";
-import { connect } from "react-redux";
+import { assert } from "@itwin/core-bentley";
+import type { CommonProps } from "@itwin/core-react";
+import { Label, Select } from "@itwin/itwinui-react";
 import { UiFramework } from "../UiFramework";
 import { FooterIndicator } from "../layout/footer/Indicator";
-import type { PresentationSelectionScope } from "../redux/SessionState";
-import "./SelectionScope.scss";
 import { useTranslation } from "../hooks/useTranslation";
+import { useFrameworkState } from "../redux/useFrameworkState";
 
-/** Defines properties supported by the SelectionScopeField Component.
+/** `SelectionScopeField` component is designed to be specified in a status bar.
+ * It displays the active selection scope and the stored list of scopes from which the user can change the active selection scope.
  * @public
  */
-interface SelectionScopeFieldProps extends CommonProps {
-  activeSelectionScope: string;
-  availableSelectionScopes: PresentationSelectionScope[];
-}
-
-/**
- * Status Field React component. This component is designed to be specified in a status bar definition.
- */
-function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
+export function SelectionScopeField(props: CommonProps) {
   const { translate } = useTranslation();
+  const frameworkState = useFrameworkState();
+  assert(!!frameworkState);
+  const { activeSelectionScope, availableSelectionScopes } =
+    frameworkState.sessionState;
 
   const options = React.useMemo(
     () =>
-      props.availableSelectionScopes.map((scope) => {
+      availableSelectionScopes.map((scope) => {
         return { value: scope.id, label: scope.label };
       }),
-    [props.availableSelectionScopes]
+    [availableSelectionScopes]
   );
 
   const updateSelectValue = (newValue: string) => {
-    // istanbul ignore else
-    if (newValue) {
-      UiFramework.setActiveSelectionScope(newValue);
-    }
+    if (!newValue) return;
+    UiFramework.setActiveSelectionScope(newValue);
   };
 
   return (
@@ -60,7 +54,7 @@ function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
       </Label>
       <Select
         className="uifw-statusFields-selectionScope-selector"
-        value={props.activeSelectionScope}
+        value={activeSelectionScope}
         options={options}
         onChange={updateSelectValue}
         data-testid="components-selectionScope-selector"
@@ -70,28 +64,3 @@ function SelectionScopeFieldComponent(props: SelectionScopeFieldProps) {
     </FooterIndicator>
   );
 }
-
-/** Function used by Redux to map state data in Redux store to props that are used to render this component. */
-function mapStateToProps(state: any) {
-  const frameworkState = state[UiFramework.frameworkStateKey]; // since app sets up key, don't hard-code name
-  /* istanbul ignore next */
-  if (!frameworkState) return undefined;
-
-  return {
-    activeSelectionScope: frameworkState.sessionState.activeSelectionScope,
-    availableSelectionScopes:
-      frameworkState.sessionState.availableSelectionScopes,
-  };
-}
-
-/**
- * SelectionScopeField React component. This component is designed to be specified in a status bar definition. It will
- * display the active selection scope from `UiFramework.getActiveSelectionScope()`, and display the stored list of scopes from
- * `UiFramework.getAvailableSelectionScopes()` to allow the user to change the active selection scope, using `UiFramework.setActiveSelectionScope()`.
- * This React component is Redux connected.
- * @public
- */
-export const SelectionScopeField: ConnectedComponent<
-  typeof SelectionScopeFieldComponent,
-  CommonProps
-> = connect(mapStateToProps)(SelectionScopeFieldComponent);
