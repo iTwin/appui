7155d3a6b502674b816d45626e4310cecd34b617
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-19 22:06:31 +0300

    Fix core-react tests.

--- ui/core-react/src/test/l10n/usePackageTranslation.test.tsx
+++ ui/core-react/src/test/l10n/usePackageTranslation.test.tsx
@@ -88,7 +88,16 @@ describe("usePackageTranslation", () => {
   });
 
   it("should update translations when localization context changes", async () => {
-    const { result, rerender } = renderHook(
+    const store = {
+      localization,
+      update: (_l: Localization) => {},
+    };
+    function Wrapper(props: any) {
+      const [l, setL] = React.useState(store.localization);
+      store.update = setL;
+      return <LocalizationProvider localization={l} {...props} />;
+    }
+    const { result } = renderHook(
       () =>
         usePackageTranslation({
           namespace: "test-namespace",
@@ -96,12 +105,7 @@ describe("usePackageTranslation", () => {
           defaults: {},
         }),
       {
-        wrapper: (props: any) => (
-          <LocalizationProvider localization={localization} {...props} />
-        ),
-        initialProps: {
-          localization,
-        },
+        wrapper: (props) => <Wrapper {...props} />,
       }
     );
 
@@ -110,9 +114,7 @@ describe("usePackageTranslation", () => {
       getLocalizedString: (_key: string) => "updated-localized-string",
     };
     const spy = vi.spyOn(newLocalization, "getLocalizedString");
-    rerender({
-      localization: newLocalization,
-    });
+    store.update(newLocalization);
 
     await vi.waitFor(() => {
       expect(result.current.translate("prop.val")).to.eq(
