1eaa5e7118d4a68a11e6f13df76db43a7734680d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-05-27 13:11:49 +0300

    Fix tests.

--- ui/appui-react/src/test/theme/ThemeManager.test.tsx
+++ ui/appui-react/src/test/theme/ThemeManager.test.tsx
@@ -2,15 +2,48 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import * as React from "react";
 import { Provider } from "react-redux";
-import { UiFramework } from "../../appui-react";
+import {
+  ConfigurableUiActions,
+  FrameworkReducer,
+  UiFramework,
+} from "../../appui-react";
 import { ThemeManager } from "../../appui-react/theme/ThemeManager";
 import TestUtils from "../TestUtils";
 import { ColorTheme } from "../../appui-react/theme/ThemeId";
+import { combineReducers, createStore } from "redux";
+import { act } from "react-dom/test-utils";
 
 describe("ThemeManager", () => {
+  it("w/ redux", async () => {
+    TestUtils.terminateUiFramework();
+    const reducer = combineReducers({
+      frameworkState: FrameworkReducer,
+    });
+    const store = createStore(reducer);
+    store.dispatch(ConfigurableUiActions.setTheme("custom-theme"));
+    await UiFramework.initialize(store);
+
+    const { container } = render(<ThemeManager />, {
+      wrapper: (props: any) => (
+        <Provider store={UiFramework.store} {...props} />
+      ),
+    });
+    expect(
+      container.ownerDocument.documentElement.getAttribute("data-theme")
+    ).toEqual("custom-theme");
+  });
+
+  it("w/o redux", () => {
+    TestUtils.terminateUiFramework();
+    const { container } = render(<ThemeManager />);
+    expect(
+      container.ownerDocument.documentElement.getAttribute("data-theme")
+    ).toEqual("SYSTEM_PREFERRED");
+  });
+
   it("should change the theme", () => {
     const { container } = render(
       <Provider store={TestUtils.store}>
@@ -19,7 +52,9 @@ describe("ThemeManager", () => {
         </ThemeManager>
       </Provider>
     );
-    UiFramework.setColorTheme(ColorTheme.Dark);
+    act(() => {
+      UiFramework.setColorTheme(ColorTheme.Dark);
+    });
     expect(UiFramework.getColorTheme()).toEqual(ColorTheme.Dark);
     expect(
       container.ownerDocument.documentElement.getAttribute("data-theme")
@@ -28,45 +63,5 @@ describe("ThemeManager", () => {
       `[data-root-container="appui-root-id"]`
     )!;
     expect(providerDiv.getAttribute("data-iui-theme")).toEqual("dark");
-  });
-
-  it("should change the widget opacity", async () => {
-    const { container } = render(
-      <Provider store={TestUtils.store}>
-        <ThemeManager>
-          <div>Hello World!</div>
-        </ThemeManager>
-      </Provider>
-    );
-    const testValue = 0.699;
-    UiFramework.setWidgetOpacity(testValue);
-    expect(UiFramework.getWidgetOpacity()).toEqual(testValue);
-    await waitFor(() => {
-      expect(
-        container.ownerDocument.documentElement.style.getPropertyValue(
-          "--buic-widget-opacity"
-        )
-      ).toEqual("0.699");
-    });
-  });
-
-  it("should change the toolbar opacity", async () => {
-    const { container } = render(
-      <Provider store={TestUtils.store}>
-        <ThemeManager>
-          <div>Hello World!</div>
-        </ThemeManager>
-      </Provider>
-    );
-    const testValue = 0.822;
-    UiFramework.setToolbarOpacity(testValue);
-    expect(UiFramework.getToolbarOpacity()).toEqual(testValue);
-    await waitFor(() => {
-      expect(
-        container.ownerDocument.documentElement.style.getPropertyValue(
-          "--buic-toolbar-opacity"
-        )
-      ).toEqual("0.822");
-    });
   });
 });
