49cf7da48cca8a5b2e8b2c93fa124f41ac89e4ad
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-06-13 11:04:54 +0300

    Update ui/appui-react/src/appui-react/framework/FrameworkBackstage.ts
    Co-authored-by: Ignas Kasinskas <138560995+ignas-k@users.noreply.github.com>

--- ui/appui-react/src/appui-react/framework/FrameworkBackstage.ts
+++ ui/appui-react/src/appui-react/framework/FrameworkBackstage.ts
@@ -43,7 +43,7 @@ export interface FrameworkBackstage {
    * Toggle the backstage.
    */
   toggle(): void;
-  /** Creates a CommandItemDef that toggle the backstage.
+  /** Creates a CommandItemDef that toggles the backstage.
    * @param overrideIconSpec Icon to replace the default 'home'.
    * @deprecated in 4.15.0. Use {@link BackstageAppButton} component or {@link FrameworkBackstage.toggle} instead.
    */
