2fb85960db4730dbcbc90f0ae1ebd4e1e1af7cfb
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-09-17 13:50:33 +0300

    Add ConditionalValue

--- ui/appui-react/src/appui-react/shared/ConditionalValue.ts
+++ ui/appui-react/src/appui-react/shared/ConditionalValue.ts
@@ -11,6 +11,15 @@ import {
 } from "@itwin/appui-abstract";
 import { ConditionalIconItem as _ConditionalIconItem } from "@itwin/core-react";
 
+/** Conditional value interface that holds the value of a generic type `T`. The `getValue` function should be called when sync event is emitted that matches one of specified `eventIds` values.
+ * @note Use {@link useConditionalValue} hook to get the value.
+ * @public
+ */
+export interface ConditionalValue<T> {
+  eventIds: string[];
+  getValue: () => T;
+}
+
 /** Class used to return a boolean value. The boolean value is refreshed by using the specified function. The `syncEventIds` define
  * events that would require the `testFunc` function to be rerun.
  * @public
