1c7005abeba842369af9474027b9e70aadfb1cbd
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-09-25 14:33:52 +0300

    re-use cjs-package script to add CJS specific package.json

--- common/scripts/cjs-package.mjs
+++ common/scripts/cjs-package.mjs
@@ -3,9 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import fs from "node:fs";
+import path from "node:path";
+
+const relativeDir = process.argv[2];
+const packageDir = path.join(process.cwd(), relativeDir);
+const filePath = path.join(packageDir, "package.json");
 
 try {
-  fs.writeFileSync("lib/package.json", '{ "type": "commonjs" }');
+  fs.writeFileSync(filePath, '{ "type": "commonjs" }');
 } catch (e) {
   console.error("Cannot create package.json", e);
 }
