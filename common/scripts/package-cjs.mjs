0545b3bf57acfbb78b7862094988d6addbc51eab
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-09-27 11:15:26 +0300

    package-cjs to create dir if needed

--- common/scripts/package-cjs.mjs
+++ common/scripts/package-cjs.mjs
@@ -10,6 +10,9 @@ const packageDir = path.join(process.cwd(), relativeDir);
 const filePath = path.join(packageDir, "package.json");
 
 try {
+  if (!fs.existsSync(packageDir)) {
+    fs.mkdirSync(packageDir, { recursive: true });
+  }
   fs.writeFileSync(filePath, '{ "type": "commonjs" }', {});
 } catch (e) {
   console.error("Cannot create package.json", e);
