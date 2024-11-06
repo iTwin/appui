22c63c2a1e75fe964ffe6d34183cc00bd47df8c9
Martynas Budrevičius <martynas.budrevicius@bentley.com>
2024-11-06 17:03:51 +0200

    PR comment fixes.

--- ui/appui-react/src/appui-react/icons/SvgSelectionClear.tsx
+++ ui/appui-react/src/appui-react/icons/SvgSelectionClear.tsx
@@ -11,11 +11,7 @@ import * as React from "react";
 /** @internal */
 export function SvgSelectionClear() {
   return (
-    <svg
-      data-name="Calque 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
       <path d="M2 5H1v2h1Zm0 3H1v2h1Zm7-6H7v1h2ZM6 2H4v1h2ZM2 3h1V2H1v2h1Zm12 7h1V8h-1Zm0 2h-1v1h2v-2h-1Zm0-5h1V5h-1Zm-7 6h2v-1H7Zm3 0h2v-1h-2Zm-6.846.75L14.25 2.654 12.846 1.25 1.75 12.346Z" />
     </svg>
   );
--- ui/appui-react/src/appui-react/icons/cube-faces/SvgCubeFaceFront.tsx
+++ ui/appui-react/src/appui-react/icons/cube-faces/SvgCubeFaceFront.tsx
@@ -12,11 +12,7 @@ import * as React from "react";
 export function SvgCubeFaceFront() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-      <path
-        opacity=".33"
-        style={{ isolation: "isolate" }}
-        d="M5.5 5.5v10.06l9.94-2.12V3.56z"
-      />
+      <path opacity=".33" d="M5.5 5.5v10.06l9.94-2.12V3.56z" />
       <path d="M11 0h-1L0 2v11l5 3h1l10-2V3Zm4 12.62-4-2.19V5l4-.8Zm-.49-9.35L11 4V1.17ZM10 1v3.19L6 5 1.69 2.68Zm0 4.2V10l-4 .8V6Zm-5 .36V11l-4 .8V3.37Zm-3.51 7.17L5 12v2.82ZM6 11.81l4-.81 4.32 2.32L6 15Z" />
     </svg>
   );
