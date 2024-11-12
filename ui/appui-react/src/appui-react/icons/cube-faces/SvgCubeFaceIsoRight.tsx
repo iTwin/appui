d855471cd3ef074611fcbe75566916cbcd1404c1
MBudreviciusBentley <Martynas.Budrevicius@bentley.com>
2024-11-12 10:32:17 +0200

    Remove icons-generic (#1074)
    * Removed a lot of dependencies on @bentley/icons-generic
    
    * Updated e2e tests to use relative path
    
    ---------
    
    Co-authored-by: GerardasB <10091419+GerardasB@users.noreply.github.com>
    (cherry picked from commit 2810fd052b0d57496f2214d7161bb8914e95ec71)
    
    # Conflicts:
    #	common/config/rush/pnpm-lock.yaml
    #	ui/appui-react/src/appui-react/statusfields/SectionsField.tsx
    #	ui/components-react/package.json
    #	ui/components-react/src/components-react/editors/EnumButtonGroupEditor.tsx

---
+++ ui/appui-react/src/appui-react/icons/cube-faces/SvgCubeFaceIsoRight.tsx
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Utilities
+ */
+
+import * as React from "react";
+
+/** @internal */
+export function SvgCubeFaceIsoRight() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+      <path opacity=".33" d="m12.165 1.176-.941 3.344 4.485 4.846z" />
+      <path d="M13.052 1.231 11 0h-1L0 2v11l5 3h1l10-2V8.043Zm-.829.669.04.024L15 8.25l-3.5-3.781Zm-.889-.533L11 2.554V1.166ZM5 14.834l-3.51-2.107L5 12.016Zm0-3.83-4 .805V3.37l4 2.192Zm-3.314-8.32L10 1.02v3.168L6 5ZM10 5.196V10l-4 .804V6ZM6 14.98v-3.168L10 11l4.322 2.316Zm5-4.543V5.401l4 4.321v2.902Z" />
+    </svg>
+  );
+}
