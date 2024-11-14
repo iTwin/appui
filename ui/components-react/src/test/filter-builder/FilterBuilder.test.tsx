66f79b7cb3f8e8a55b5f600fe0b11c6c7c795ef5
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-11-14 14:10:56 +0200

    Move `@itwin/itwinui-react` to peerDependencies (#1115)
    * Move itwinui-react to peer deps.
    
    * Remove outdated package.json comments
    
    * Remove @itwin/itwinui-variables
    
    * rush change
    
    * Fix FilterBuilder tests
    
    * Update snaps
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>

--- ui/components-react/src/test/filter-builder/FilterBuilder.test.tsx
+++ ui/components-react/src/test/filter-builder/FilterBuilder.test.tsx
@@ -27,6 +27,26 @@ describe("PropertyFilterBuilder", () => {
     typename: "string",
   };
 
+  beforeEach(() => {
+    vi.spyOn(window.Element.prototype, "getBoundingClientRect").mockReturnValue(
+      {
+        height: 20,
+        width: 20,
+        x: 0,
+        y: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        top: 0,
+        toJSON: () => {},
+      }
+    );
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   it("call onFilterChanged with filter after new rule is setup", async () => {
     const user = userEvent.setup();
     const spy = vi.fn();
