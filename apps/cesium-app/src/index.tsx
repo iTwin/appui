7900a690071d35eaded4901fd674c9eb440e509d
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-11-22 11:35:38 +0200

    Add cesium-app

---
+++ apps/cesium-app/src/index.tsx
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
+
+function App() {
+  return <StrictMode>Test App</StrictMode>;
+}
