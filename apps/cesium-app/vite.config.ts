ab676b088ffa5df2bdb996e0c31a42e4f2ac2721
GerardasB <10091419+GerardasB@users.noreply.github.com>
2024-11-25 16:34:50 +0200

    Change cesium base url.

--- apps/cesium-app/vite.config.ts
+++ apps/cesium-app/vite.config.ts
@@ -6,19 +6,20 @@ import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const cesiumSource = "node_modules/cesium/Build/Cesium";
+const cesiumBaseUrl = "public";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    CESIUM_BASE_URL: JSON.stringify(`/`),
+    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
   },
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: `${cesiumSource}/ThirdParty`, dest: "." },
-        { src: `${cesiumSource}/Workers`, dest: "." },
-        { src: `${cesiumSource}/Assets`, dest: "." },
-        { src: `${cesiumSource}/Widgets`, dest: "." },
+        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
       ],
     }),
   ],
