800ce7d8c79abdb21067925dea633825301e04e8
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-01-20 17:52:19 +0200

    Add new editors to sample frontstage

--- apps/test-providers/src/ui/components/EditorExampleComponent.tsx
+++ apps/test-providers/src/ui/components/EditorExampleComponent.tsx
@@ -20,7 +20,7 @@ import {
   SliderEditorParams,
   StandardTypeNames as Type,
 } from "@itwin/appui-abstract";
-import { EditorContainer } from "@itwin/components-react";
+import { EditorContainer, PropertyRecordEditor } from "@itwin/components-react";
 import {
   DropdownMenu,
   Flex,
@@ -89,164 +89,202 @@ const inputEditorSizeParams = {
   maxLength: 5,
 } as InputEditorSizeParams;
 
+const propertyRecords = [
+  createPropertyRecord(Type.String, "hi"),
+  createPropertyRecord(Type.String, "hi", {
+    name: Editor.MultiLine,
+    params: [
+      {
+        type: EditorParam.MultilineText,
+        rows: 5,
+      } as MultilineTextEditorParams,
+    ],
+  }),
+  // BROKEN!
+  // createPropertyRecord(Type.String, "icon-app-2", {
+  //   name: Editor.IconPicker,
+  //   params: [
+  //     {
+  //       type: EditorParam.IconListData,
+  //       iconValue: "icon-app-2",
+  //       numColumns: 2,
+  //       iconValues: ["icon-app-1", "icon-app-2", "icon-apps-itwin"],
+  //     } as IconListEditorParams,
+  //   ],
+  // }),
+  createPropertyRecord(Type.DateTime, new Date(2018, 0, 1)),
+  createPropertyRecord(Type.ShortDate, new Date(2018, 0, 1)),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.Slider,
+    params: [
+      {
+        type: EditorParam.Slider,
+        minimum: 0,
+        maximum: 10,
+      } as SliderEditorParams,
+    ],
+  }),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.NumberCustom,
+    params: [customFormattedNumberParams],
+  }),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.NumberCustom,
+    params: [
+      customFormattedNumberParams,
+      {
+        type: EditorParam.Icon,
+        definition: { iconSpec: "icon-placeholder" },
+      } as IconEditorParams,
+    ],
+  }),
+  createPropertyRecord(Type.Number, 1, Editor.NumericInput),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.NumericInput,
+    params: [inputEditorSizeParams],
+  }),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.NumericInput,
+    params: [
+      {
+        type: EditorParam.Range,
+        minimum: 0,
+        maximum: 10,
+        step: 0.5,
+        precision: 1,
+      } as RangeEditorParams,
+    ],
+  }),
+  createPropertyRecord(Type.Number, 1, {
+    name: Editor.NumericInput,
+    params: [
+      inputEditorSizeParams,
+      {
+        type: EditorParam.Range,
+        minimum: 0,
+        maximum: 10,
+        step: 0.25,
+        precision: 2,
+      } as RangeEditorParams,
+    ],
+  }),
+  createPropertyRecord(Type.Boolean, true),
+  createPropertyRecord(Type.Boolean, true, Editor.Toggle),
+  createPropertyRecord(Type.Boolean, true, {
+    name: "image-check-box",
+    params: [
+      {
+        type: EditorParam.CheckBoxImages,
+        imageOff: "icon-visibility-hide-2",
+        imageOn: "icon-visibility",
+      } as ImageCheckBoxParams,
+    ],
+  }),
+  createEnumProperty(),
+  createEnumProperty(Editor.EnumButtonGroup),
+  createEnumProperty({
+    name: Editor.EnumButtonGroup,
+    params: [
+      {
+        type: EditorParam.ButtonGroupData,
+        buttons: [
+          {
+            iconSpec: "icon-app-1",
+          },
+          {
+            iconSpec: "icon-app-2",
+          },
+          {
+            iconSpec: "icon-apps-itwin",
+          },
+        ],
+      } as ButtonGroupEditorParams,
+    ],
+  }),
+];
+
 /** Component that display at least 1 of each variety of editors registered by default in Components-react. */
 export function EditorExampleComponent() {
   return (
-    <Flex flexDirection="column" alignItems="flex-start" gap="m">
-      {[
-        createPropertyRecord(Type.String, "hi"),
-        createPropertyRecord(Type.String, "hi", {
-          name: Editor.MultiLine,
-          params: [
-            {
-              type: EditorParam.MultilineText,
-              rows: 5,
-            } as MultilineTextEditorParams,
-          ],
-        }),
-        // BROKEN!
-        // createPropertyRecord(Type.String, "icon-app-2", {
-        //   name: Editor.IconPicker,
-        //   params: [
-        //     {
-        //       type: EditorParam.IconListData,
-        //       iconValue: "icon-app-2",
-        //       numColumns: 2,
-        //       iconValues: ["icon-app-1", "icon-app-2", "icon-apps-itwin"],
-        //     } as IconListEditorParams,
-        //   ],
-        // }),
-        createPropertyRecord(Type.DateTime, new Date(2018, 0, 1)),
-        createPropertyRecord(Type.ShortDate, new Date(2018, 0, 1)),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.Slider,
-          params: [
-            {
-              type: EditorParam.Slider,
-              minimum: 0,
-              maximum: 10,
-            } as SliderEditorParams,
-          ],
-        }),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.NumberCustom,
-          params: [customFormattedNumberParams],
-        }),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.NumberCustom,
-          params: [
-            customFormattedNumberParams,
-            {
-              type: EditorParam.Icon,
-              definition: { iconSpec: "icon-placeholder" },
-            } as IconEditorParams,
-          ],
-        }),
-        createPropertyRecord(Type.Number, 1, Editor.NumericInput),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.NumericInput,
-          params: [inputEditorSizeParams],
-        }),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.NumericInput,
-          params: [
-            {
-              type: EditorParam.Range,
-              minimum: 0,
-              maximum: 10,
-              step: 0.5,
-              precision: 1,
-            } as RangeEditorParams,
-          ],
-        }),
-        createPropertyRecord(Type.Number, 1, {
-          name: Editor.NumericInput,
-          params: [
-            inputEditorSizeParams,
-            {
-              type: EditorParam.Range,
-              minimum: 0,
-              maximum: 10,
-              step: 0.25,
-              precision: 2,
-            } as RangeEditorParams,
-          ],
-        }),
-        createPropertyRecord(Type.Boolean, true),
-        createPropertyRecord(Type.Boolean, true, Editor.Toggle),
-        createPropertyRecord(Type.Boolean, true, {
-          name: "image-check-box",
-          params: [
-            {
-              type: EditorParam.CheckBoxImages,
-              imageOff: "icon-visibility-hide-2",
-              imageOn: "icon-visibility",
-            } as ImageCheckBoxParams,
-          ],
-        }),
-        createEnumProperty(),
-        createEnumProperty(Editor.EnumButtonGroup),
-        createEnumProperty({
-          name: Editor.EnumButtonGroup,
-          params: [
-            {
-              type: EditorParam.ButtonGroupData,
-              buttons: [
-                {
-                  iconSpec: "icon-app-1",
-                },
-                {
-                  iconSpec: "icon-app-2",
-                },
-                {
-                  iconSpec: "icon-apps-itwin",
-                },
-              ],
-            } as ButtonGroupEditorParams,
-          ],
-        }),
-      ].map((record) => {
+    <Flex
+      flexDirection="column"
+      alignItems="flex-start"
+      gap="m"
+      style={{ width: "100%" }}
+    >
+      {propertyRecords.map((record) => {
         const key = `${PropertyValueFormat[record.value.valueFormat]}:${
           record.property.typename
         }:${record.property.editor?.name ?? "Default"}[${
           record.property.editor?.params?.map((p) => p.type).join(",") ?? ""
         }]`.replace("[]", "");
         return (
-          <Flex.Item key={key} id={key.replace(/[^A-Za-z]/g, "")}>
-            <Flex flexDirection="row" flexWrap="nowrap" alignItems="flex-end">
-              {availableSizes.map((localSize) => (
-                <Flex.Item key={localSize}>
-                  <EditorContainer
-                    propertyRecord={record}
-                    onCommit={() => undefined}
-                    onCancel={() => undefined}
-                    // Use when merging #576 size={localSize === "small" ? undefined : localSize}
-                  />
-                </Flex.Item>
-              ))}
+          <Flex key={key} flexDirection="column" style={{ width: "100%" }}>
+            <Flex flexDirection="row" gap="xl" style={{ width: "100%" }}>
+              <Flex.Item alignSelf="flex-start" style={{ width: "100px" }}>
+                <OldEditorRenderer record={record} />
+              </Flex.Item>
+              <Flex.Item alignSelf="flex-end" style={{ width: "100px" }}>
+                <NewEditorRenderer record={record} />
+              </Flex.Item>
             </Flex>
-            <Text variant="small" isMuted>
-              {key}
-              {record.property.editor && (
-                <DropdownMenu
-                  menuItems={(close) => [
-                    <MenuItem key={1} onClick={close}>
-                      <Text variant="leading">Editor config:</Text>
-                      <code style={{ whiteSpace: "pre", display: "block" }}>
-                        {JSON.stringify(record.property.editor, undefined, 2)}
-                      </code>
-                    </MenuItem>,
-                  ]}
-                >
-                  <IconButton styleType="borderless" size="small">
-                    <SvgDetails />
-                  </IconButton>
-                </DropdownMenu>
-              )}
-            </Text>
-          </Flex.Item>
+            <Flex.Item>
+              <Text variant="small" isMuted>
+                {record.property.editor && (
+                  <DropdownMenu
+                    menuItems={(close) => [
+                      <MenuItem key={1} onClick={close}>
+                        <Text variant="leading">Editor config:</Text>
+                        <code style={{ whiteSpace: "pre", display: "block" }}>
+                          {JSON.stringify(record.property.editor, undefined, 2)}
+                        </code>
+                      </MenuItem>,
+                    ]}
+                  >
+                    <IconButton styleType="borderless" size="small">
+                      <SvgDetails />
+                    </IconButton>
+                  </DropdownMenu>
+                )}
+              </Text>
+            </Flex.Item>
+          </Flex>
         );
       })}
+    </Flex>
+  );
+}
+
+function OldEditorRenderer({ record }: { record: PropertyRecord }) {
+  return (
+    <Flex flexDirection="row" flexWrap="nowrap" alignItems="flex-end">
+      {availableSizes.map((localSize) => (
+        <Flex.Item key={localSize}>
+          <EditorContainer
+            propertyRecord={record}
+            onCommit={() => undefined}
+            onCancel={() => undefined}
+            // Use when merging #576 size={localSize === "small" ? undefined : localSize}
+          />
+        </Flex.Item>
+      ))}
+    </Flex>
+  );
+}
+
+function NewEditorRenderer({ record }: { record: PropertyRecord }) {
+  return (
+    <Flex flexDirection="row" flexWrap="nowrap" alignItems="flex-end">
+      {availableSizes.map((localSize) => (
+        <Flex.Item key={localSize}>
+          <PropertyRecordEditor
+            propertyRecord={record}
+            onCommit={() => undefined}
+            onCancel={() => undefined}
+            // size={localSize}
+          />
+        </Flex.Item>
+      ))}
     </Flex>
   );
 }
