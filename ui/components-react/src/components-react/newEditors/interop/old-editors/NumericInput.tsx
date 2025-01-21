3f5b6d06920b54e6fee6d315c835511ea2ae12f1
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-01-21 18:21:35 +0200

    Improve range params handling in NumericInput

--- ui/components-react/src/components-react/newEditors/interop/old-editors/NumericInput.tsx
+++ ui/components-react/src/components-react/newEditors/interop/old-editors/NumericInput.tsx
@@ -38,13 +38,15 @@ function NumericInputEditor({
 }: EditorProps<OldEditorMetadata, NumericValue>) {
   const sizeParams = useInputEditorSizeParams(metadata);
   const rangeParams = useRangeEditorParams(metadata);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = event.target.value;
+  const handleChange = (newValue: string) => {
     onChange({
-      displayValue: currentValue,
-      rawValue: parseFloat(currentValue),
+      displayValue: newValue,
+      rawValue: parseFloat(newValue),
     });
+  };
+
+  const handleBlur = () => {
+    onFinish();
   };
 
   const style: React.CSSProperties | undefined =
@@ -54,18 +56,84 @@ function NumericInputEditor({
         }
       : undefined;
 
+  const inputProps = useNumericInput({
+    min: rangeParams?.minimum,
+    max: rangeParams?.maximum,
+    precision: rangeParams?.precision,
+    step: rangeParams?.step,
+    maxLength: sizeParams?.maxLength,
+    onChange: handleChange,
+    onBlur: handleBlur,
+    value: value?.displayValue ?? "",
+  });
+
   return (
-    <Input
-      style={style}
-      type="number"
-      min={rangeParams?.minimum}
-      max={rangeParams?.maximum}
-      step={rangeParams?.step}
-      onChange={handleChange}
-      value={value?.displayValue}
-      size={size}
-      disabled={disabled}
-      onBlur={onFinish}
-    />
+    <Input style={style} size={size} disabled={disabled} {...inputProps} />
   );
+}
+
+interface UseNumericInputProps {
+  min?: number;
+  max?: number;
+  step?: number;
+  precision?: number;
+  maxLength?: number;
+  value: string;
+  onChange: (newValue: string) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+function useNumericInput({
+  min,
+  max,
+  step,
+  precision,
+  maxLength,
+  value,
+  onChange,
+  onBlur,
+}: UseNumericInputProps) {
+  const formatValue = (val: number) => {
+    const appliedMin = min ?? Number.MIN_VALUE;
+    const appliedMax = max ?? Number.MAX_VALUE;
+    const valueInRange = Math.min(appliedMax, Math.max(appliedMin, val));
+
+    return precision
+      ? valueInRange.toFixed(precision)
+      : valueInRange.toString();
+  };
+
+  const [formattedValue, setFormattedValue] = React.useState(() => {
+    return value.length !== 0 ? formatValue(Number(value)) : value;
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const currentValue = event.target.value;
+    if (maxLength && currentValue.length > maxLength) {
+      return;
+    }
+    setFormattedValue(currentValue);
+    onChange(currentValue);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (precision) {
+      const newFormattedValue = formatValue(Number(formattedValue));
+      if (newFormattedValue !== formattedValue) {
+        setFormattedValue(newFormattedValue);
+        onChange(newFormattedValue);
+      }
+    }
+    onBlur?.(e);
+  };
+
+  return {
+    type: "number",
+    min,
+    max,
+    step,
+    value: formattedValue,
+    onChange: handleChange,
+    onBlur: handleBlur,
+  };
 }
