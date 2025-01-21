52aba5099edaf6f6d2a7fcdbe59c1940f6d5c83d
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-01-21 18:21:54 +0200

    TextArea editor aka Multiline

---
+++ ui/components-react/src/components-react/newEditors/interop/old-editors/TextArea.tsx
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as React from "react";
+import { Button, Popover, Textarea } from "@itwin/itwinui-react";
+import { createEditorSpec, type EditorProps } from "../../Types.js";
+import { isOldEditorMetadata, type OldEditorMetadata } from "../Metadata.js";
+import { type TextValue, Value } from "../../values/Values.js";
+import { StandardEditorNames } from "@itwin/appui-abstract";
+import type { ValueMetadata } from "../../values/Metadata.js";
+
+/** @internal */
+export const MultilineEditorSpec = createEditorSpec({
+  isMetadataSupported: (
+    metadata: ValueMetadata
+  ): metadata is OldEditorMetadata =>
+    isOldEditorMetadata(metadata) && metadata.type === "string",
+  isValueSupported: Value.isTextValue,
+  applies: (metadata) =>
+    metadata.preferredEditor === StandardEditorNames.MultiLine,
+  Editor: TextAreaEditor,
+});
+
+function TextAreaEditor({
+  value,
+  onChange,
+  onFinish,
+  size,
+  disabled,
+}: EditorProps<OldEditorMetadata, TextValue>) {
+  const currentValue = value ?? { value: "" };
+
+  return (
+    <Popover
+      placement="bottom"
+      content={
+        <Textarea
+          value={currentValue.value}
+          onChange={(e) => {
+            const newValue = e.currentTarget.value;
+            onChange({ value: newValue });
+          }}
+        />
+      }
+      onVisibleChange={(visible) => {
+        if (!visible) {
+          onFinish();
+        }
+      }}
+    >
+      <Button style={{ width: "100%" }} size={size} disabled={disabled}>
+        {currentValue.value}
+      </Button>
+    </Popover>
+  );
+}
