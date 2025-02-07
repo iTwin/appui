9e9ce422f3a4c6fb34ddd74b9e8204260b86cc9a
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-02-07 15:54:39 +0200

    Refactor cursor prompt.

--- ui/appui-react/src/appui-react/cursor/cursorprompt/CursorPrompt.tsx
+++ ui/appui-react/src/appui-react/cursor/cursorprompt/CursorPrompt.tsx
@@ -8,10 +8,7 @@
 
 import "./CursorPrompt.scss";
 import * as React from "react";
-import type { ToolAssistanceInstruction } from "@itwin/core-frontend";
-import type { XAndY } from "@itwin/core-geometry";
 import { RelativePosition } from "@itwin/appui-abstract";
-import type { ListenerType } from "@itwin/core-react/internal";
 import { Icon, Timer } from "@itwin/core-react";
 import { Point } from "@itwin/core-react/internal";
 import { Text } from "@itwin/itwinui-react";
@@ -20,47 +17,39 @@ import { CursorPopupManager } from "../cursorpopup/CursorPopupManager.js";
 
 /** @internal */
 export class CursorPrompt {
-  private _fadeOut: boolean;
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  private _timer?: Timer;
-  private _relativePosition = RelativePosition.BottomRight;
-  private _offset: Point = new Point(20, 20);
   private _popupId = "cursor-prompt";
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
+  private _timer: Timer | undefined;
+  private _removeListeners: (() => void) | undefined;
 
-  constructor(timeOut: number, fadeOut: boolean) {
-    this._fadeOut = fadeOut;
-
-    if (timeOut === Number.POSITIVE_INFINITY) return;
-
-    // eslint-disable-next-line @typescript-eslint/no-deprecated
-    this._timer = new Timer(timeOut);
-    this._timer.setOnExecute(() => this.close());
-  }
-
-  public display(
-    toolIconSpec: string,
-    instruction: ToolAssistanceInstruction,
-    offset: XAndY = { x: 20, y: 20 },
-    relativePosition: RelativePosition = RelativePosition.BottomRight
-  ) {
-    if (!instruction.text) {
-      if (this._timer?.isRunning) this.close();
-      return;
+  public open({
+    timeout,
+    fadeout,
+    iconSpec,
+    instruction,
+  }: {
+    timeout: number;
+    fadeout: boolean;
+    iconSpec: string;
+    instruction: string;
+  }) {
+    if (!this._removeListeners) {
+      this._removeListeners =
+        CursorInformation.onCursorUpdatedEvent.addListener((args) => {
+          CursorPopupManager.updatePosition(args.newPt);
+        });
     }
-
-    this._relativePosition = relativePosition;
-    this._offset = Point.create(offset);
 
     const promptElement = (
       <div className="uifw-cursor-prompt">
-        {toolIconSpec && (
+        {iconSpec && (
           <span className="uifw-cursor-prompt-icon">
             {/* eslint-disable-next-line @typescript-eslint/no-deprecated */}
-            <Icon iconSpec={toolIconSpec} />
+            <Icon iconSpec={iconSpec} />
           </span>
         )}
         <Text variant="body" className="uifw-cursor-prompt-text">
-          {instruction.text}
+          {instruction}
         </Text>
       </div>
     );
@@ -69,30 +58,27 @@ export class CursorPrompt {
       this._popupId,
       promptElement,
       CursorInformation.cursorPosition,
-      this._offset,
-      this._relativePosition,
+      new Point(20, 20),
+      RelativePosition.BottomRight,
       0,
       { shadow: true }
     );
-    if (!CursorInformation.onCursorUpdatedEvent.has(this._handleCursorUpdated))
-      CursorInformation.onCursorUpdatedEvent.addListener(
-        this._handleCursorUpdated
-      );
 
-    this._timer?.start();
+    if (timeout === Number.POSITIVE_INFINITY) return;
+
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    const timer = new Timer(timeout);
+    timer.setOnExecute(() => this.close(fadeout));
+    timer.start();
+    this._timer = timer;
   }
 
-  public close() {
+  public close(fadeout: boolean) {
     this._timer?.stop();
-    CursorPopupManager.close(this._popupId, false, this._fadeOut);
-    CursorInformation.onCursorUpdatedEvent.removeListener(
-      this._handleCursorUpdated
-    );
-  }
+    this._timer = undefined;
+    this._removeListeners?.();
+    this._removeListeners = undefined;
 
-  private _handleCursorUpdated: ListenerType<
-    typeof CursorInformation.onCursorUpdatedEvent
-  > = (args) => {
-    CursorPopupManager.updatePosition(args.newPt);
-  };
+    CursorPopupManager.close(this._popupId, false, fadeout);
+  }
 }
--- ui/appui-react/src/appui-react/statusfields/toolassistance/ToolAssistanceField.tsx
+++ ui/appui-react/src/appui-react/statusfields/toolassistance/ToolAssistanceField.tsx
@@ -152,10 +152,7 @@ export class ToolAssistanceField extends React.Component<
     };
 
     this._uiSettingsStorage = new LocalStateStorage();
-    this._cursorPrompt = new CursorPrompt(
-      this.props.cursorPromptTimeout,
-      this.props.fadeOutCursorPrompt
-    );
+    this._cursorPrompt = new CursorPrompt();
     // eslint-disable-next-line @typescript-eslint/no-deprecated
     this._showPromptAtCursorSetting = new UiStateEntry(
       ToolAssistanceField._toolAssistanceKey,
@@ -188,7 +185,7 @@ export class ToolAssistanceField extends React.Component<
 
   public override componentWillUnmount() {
     this._isMounted = false;
-    this._cursorPrompt.close();
+    this._cursorPrompt.close(this.props.fadeOutCursorPrompt);
     MessageManager.onToolAssistanceChangedEvent.removeListener(
       this._handleToolAssistanceChangedEvent
     );
@@ -297,17 +294,29 @@ export class ToolAssistanceField extends React.Component<
     typeof UiFramework.frontstages.onToolIconChangedEvent
   > = (args) => {
     if (this._isMounted)
-      this.setState({ toolIconSpec: args.iconSpec }, () => {
-        this._showCursorPrompt();
-      });
+      this.setState(
+        {
+          toolIconSpec: args.iconSpec,
+        },
+        () => {
+          this._showCursorPrompt();
+        }
+      );
   };
 
   private _showCursorPrompt() {
-    if (this.state.showPromptAtCursor && this.state.instructions)
-      this._cursorPrompt.display(
-        this.state.toolIconSpec,
-        this.state.instructions.mainInstruction
-      );
+    const instruction = this.state.instructions?.mainInstruction.text;
+    if (!this.state.showPromptAtCursor || !instruction) {
+      this._cursorPrompt.close(this.props.fadeOutCursorPrompt);
+      return;
+    }
+
+    this._cursorPrompt.open({
+      timeout: this.props.cursorPromptTimeout,
+      fadeout: this.props.fadeOutCursorPrompt,
+      iconSpec: this.state.toolIconSpec,
+      instruction,
+    });
   }
 
   private _sectionHasDisplayableInstructions(
