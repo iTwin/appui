7547a154af7875617b410e9e83141ff6682ae01c
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-05-07 11:15:00 +0300

    Fix docs build?

--- ui/components-react/src/components-react/common/DisposePolyfill.ts
+++ ui/components-react/src/components-react/common/DisposePolyfill.ts
@@ -2,6 +2,9 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Common
+ */
 
 /**
  * Polyfill for upcoming resource management feature: https://github.com/tc39/proposal-explicit-resource-management
