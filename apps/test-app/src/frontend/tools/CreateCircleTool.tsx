003cd8d0e9191a7c9a74417f704c02f74d3af2b5
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-05-14 19:45:08 +0300

    Update CreateCircleTool

--- apps/test-app/src/frontend/tools/CreateCircleTool.tsx
+++ apps/test-app/src/frontend/tools/CreateCircleTool.tsx
@@ -10,7 +10,6 @@ import {
   Code,
   ElementGeometry,
   FlatBufferGeometryStream,
-  GeometricElementProps,
   JsonGeometryStream,
   PlacementProps,
 } from "@itwin/core-common";
@@ -18,13 +17,12 @@ import {
   AccuDrawHintBuilder,
   BeButton,
   BeButtonEvent,
+  IModelApp,
   LengthDescription,
   Viewport,
 } from "@itwin/core-frontend";
 import { Arc3d, Point3d, YawPitchRollAngles } from "@itwin/core-geometry";
-import { Id64String } from "@itwin/core-bentley";
 import {
-  DialogItem,
   DialogProperty,
   DialogPropertySyncItem,
   PropertyDescriptionHelper,
@@ -35,7 +33,7 @@ import {
  */
 class CreateCircleToolBase extends CreateElementWithDynamicsTool {
   public static override toolId = "CreateCircle";
-  private _point?: Point3d;
+  private _points: Point3d[] = [];
   private _current?: Arc3d;
 
   private _useRadiusProperty: DialogProperty<boolean> | undefined;
@@ -49,25 +47,30 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
   }
 
   public get useRadius() {
+    if (this._useEnteredRadius) return true;
     return this.useRadiusProperty.value;
   }
 
   private _radiusProperty: DialogProperty<number> | undefined;
   public get radiusProperty() {
-    if (!this._radiusProperty)
-      this._radiusProperty = new DialogProperty<number>(
+    if (!this._radiusProperty) {
+      this._radiusProperty = new DialogProperty(
         new LengthDescription("radius", "Radius"),
-        1,
-        undefined
+        1
       );
+    }
+
     return this._radiusProperty;
   }
 
-  public get radius(): number {
+  public get radius() {
     return this.radiusProperty.value;
   }
 
-  public override get targetCategory(): Id64String {
+  // This is set to true when the user enters a radius value in the tool settings and is reset on mouse motion.
+  private _useEnteredRadius = false;
+
+  public override get targetCategory() {
     if (!this.briefcase) {
       for (const category of this.targetView?.view.categorySelector
         .categories ?? []) {
@@ -77,7 +80,7 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
     return super.targetCategory;
   }
 
-  public override get targetModelId(): Id64String {
+  public override get targetModelId() {
     if (!this.briefcase) return "";
     return super.targetModelId;
   }
@@ -95,17 +98,17 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
     return super.isCompatibleViewport(vp, isSelectedViewChange);
   }
 
-  protected override getPlacementProps(): PlacementProps | undefined {
+  protected override getPlacementProps() {
     const vp = this.targetView;
-    if (!vp || !this._point) {
+    if (!vp || this._points.length === 0) {
       return undefined;
     }
 
-    const origin = this._point;
+    const origin = this._points[0];
     const angles = new YawPitchRollAngles();
     const matrix = AccuDrawHintBuilder.getCurrentRotation(vp, true, true);
     ElementGeometry.Builder.placementAnglesFromPoints(
-      [this._point],
+      [origin],
       matrix?.getColumn(2),
       angles
     );
@@ -128,9 +131,7 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
     return { format: "flatbuffer", data: builder.entries };
   }
 
-  protected override getElementProps(
-    placement: PlacementProps
-  ): GeometricElementProps | undefined {
+  protected override getElementProps(placement: PlacementProps) {
     return {
       classFullName: "Generic:PhysicalObject",
       model: this.targetModelId,
@@ -144,31 +145,58 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
     ev: BeButtonEvent,
     isDynamics: boolean
   ) {
-    if (!isDynamics) {
-      this._point = ev.point.clone();
+    // TODO: lock -> mouse move -> unlock should use last radius.
+    // This is simulated to generate graphics. Use entered radius until the mouse moves.
+    if (!simulated) {
+      this._useEnteredRadius = false;
     }
 
-    if (!this._point) return;
-    this._current = Arc3d.createXY(this._point, this.radius);
+    if (!isDynamics) {
+      if (this.useRadius) {
+        this._points = [ev.point.clone()];
+      } else {
+        this._points.push(ev.point.clone());
+      }
+    }
+
+    if (this._points.length === 0) return;
+
+    const center = this._points[0];
+    const edge = this._points[1] ?? ev.point;
+
+    const radius = this.useRadius
+      ? this.radius
+      : Math.max(1, center.distance(edge));
+
+    if (radius !== this.radius) {
+      this.radiusProperty.value = radius;
+      this.syncToolSettingsProperties([this.radiusProperty.syncItem]);
+    }
+
+    this._current = Arc3d.createXY(center, radius);
   }
 
-  protected override isComplete(ev: BeButtonEvent): boolean {
-    return ev.button === BeButton.Reset && !!this._point;
+  protected override isComplete(ev: BeButtonEvent) {
+    if (this.useRadius) {
+      return ev.button === BeButton.Reset && this._points.length > 0;
+    }
+
+    return ev.button === BeButton.Reset && this._points.length > 1;
   }
 
-  protected override async cancelPoint(ev: BeButtonEvent): Promise<boolean> {
+  protected override async cancelPoint(ev: BeButtonEvent) {
     if (this.isComplete(ev)) {
       await this.createElement();
     }
     return true;
   }
 
-  public override async onRestartTool(): Promise<void> {
+  public override async onRestartTool() {
     const tool = new CreateCircleTool();
     if (!(await tool.run())) return this.exitTool();
   }
 
-  public override supplyToolSettingsProperties(): DialogItem[] | undefined {
+  public override supplyToolSettingsProperties() {
     this.initializeToolSettingPropertyValues([
       this.useRadiusProperty,
       this.radiusProperty,
@@ -189,8 +217,20 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
   public override async applyToolSettingPropertyChange(
     updatedValue: DialogPropertySyncItem
   ): Promise<boolean> {
+    if (updatedValue.propertyName === this.radiusProperty.name) {
+      this._useEnteredRadius = true;
+    }
+
     if (!this.changeToolSettingPropertyValue(updatedValue)) return false;
     return true;
+  }
+
+  /** @internal */
+  protected override syncToolSettingPropertyValue(
+    property: DialogProperty<any>
+  ) {
+    // Avoid syncing the lock and disabled states: https://github.com/iTwin/itwinjs-core/blob/74603f8bd278e03a1dae74e827b51bb6f0880f85/core/frontend/src/tools/Tool.ts#L791
+    this.syncToolSettingsProperties([property.syncItem]);
   }
 
   protected override getToolSettingPropertyLocked(
@@ -199,6 +239,26 @@ class CreateCircleToolBase extends CreateElementWithDynamicsTool {
     if (property === this.useRadiusProperty) return this.radiusProperty;
     return undefined;
   }
+
+  public override async run(...args: any[]) {
+    determineSimulatedEvent();
+    return super.run(...args);
+  }
+}
+
+// Workaround to determine if the motion event is simulated.
+let simulated = false;
+let isDetermined = false;
+function determineSimulatedEvent() {
+  if (isDetermined) return;
+
+  isDetermined = true;
+  const simulateMotionEvent = IModelApp.toolAdmin.simulateMotionEvent;
+  IModelApp.toolAdmin.simulateMotionEvent = (...args) => {
+    simulated = true;
+    simulateMotionEvent.bind(IModelApp.toolAdmin)(...args);
+    simulated = false;
+  };
 }
 
 export const CreateCircleTool = ToolUtilities.defineIcon(
