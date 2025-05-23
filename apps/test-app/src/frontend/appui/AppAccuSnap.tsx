ba327026f7153e96153f62e959aa0a3651876cd0
Gerardas B. <10091419+GerardasB@users.noreply.github.com>
2025-05-23 12:24:07 +0300

    Add new snap modes (#1313)
    * Add new snap modes.
    
    * Hook-up AppAccuSnap w/ SnapModeField.
    
    * iTwin.js core 4.0 compatible
    
    * New snap modes in test-app
    
    * pnpm changeset
    
    * Fix unit tests
    
    * Add icons
    
    * Use NearestKeypoint as default

--- apps/test-app/src/frontend/appui/AppAccuSnap.tsx
+++ apps/test-app/src/frontend/appui/AppAccuSnap.tsx
@@ -3,36 +3,47 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { AccuSnap, SnapMode } from "@itwin/core-frontend";
+import { create } from "zustand";
+
+export const useAccuSnapStore = create<SnapMode>(
+  () => SnapMode.NearestKeypoint
+);
 
 export class AppAccuSnap extends AccuSnap {
   public override getActiveSnapModes(): SnapMode[] {
     const snaps: SnapMode[] = [];
 
-    const snapMode = SnapMode.Nearest; // TODO: read from store
-    if (snapMode) {
-      if ((snapMode & SnapMode.Bisector) === (SnapMode.Bisector as number))
-        snaps.push(SnapMode.Bisector);
-      if ((snapMode & SnapMode.Center) === (SnapMode.Center as number))
-        snaps.push(SnapMode.Center);
-      if (
-        (snapMode & SnapMode.Intersection) ===
-        (SnapMode.Intersection as number)
-      )
-        snaps.push(SnapMode.Intersection);
-      if ((snapMode & SnapMode.MidPoint) === (SnapMode.MidPoint as number))
-        snaps.push(SnapMode.MidPoint);
-      if ((snapMode & SnapMode.Nearest) === (SnapMode.Nearest as number))
-        snaps.push(SnapMode.Nearest);
-      if (
-        (snapMode & SnapMode.NearestKeypoint) ===
-        (SnapMode.NearestKeypoint as number)
-      )
-        snaps.push(SnapMode.NearestKeypoint);
-      if ((snapMode & SnapMode.Origin) === (SnapMode.Origin as number))
-        snaps.push(SnapMode.Origin);
-    } else {
+    const snapMode = useAccuSnapStore.getState();
+    if ((snapMode & SnapMode.Bisector) === (SnapMode.Bisector as number))
+      snaps.push(SnapMode.Bisector);
+    if ((snapMode & SnapMode.Center) === (SnapMode.Center as number))
+      snaps.push(SnapMode.Center);
+    if (
+      (snapMode & SnapMode.Intersection) ===
+      (SnapMode.Intersection as number)
+    )
+      snaps.push(SnapMode.Intersection);
+    if ((snapMode & SnapMode.MidPoint) === (SnapMode.MidPoint as number))
+      snaps.push(SnapMode.MidPoint);
+    if ((snapMode & SnapMode.Nearest) === (SnapMode.Nearest as number))
+      snaps.push(SnapMode.Nearest);
+    if (
+      (snapMode & SnapMode.NearestKeypoint) ===
+      (SnapMode.NearestKeypoint as number)
+    )
       snaps.push(SnapMode.NearestKeypoint);
-    }
+    if ((snapMode & SnapMode.Origin) === (SnapMode.Origin as number))
+      snaps.push(SnapMode.Origin);
+    if (
+      (snapMode & SnapMode.PerpendicularPoint) ===
+      (SnapMode.PerpendicularPoint as number)
+    )
+      snaps.push(SnapMode.PerpendicularPoint);
+    if (
+      (snapMode & SnapMode.TangentPoint) ===
+      (SnapMode.TangentPoint as number)
+    )
+      snaps.push(SnapMode.TangentPoint);
     return snaps;
   }
 }
---
+++ ui/appui-react/src/appui-react/icons/snaps/SvgSnapsPerpendicular.tsx
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Utilities
+ */
+
+import * as React from "react";
+
+/** @internal */
+export function SvgSnapsPerpendicular() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 2a.5.5 0 0 1 .5.5v7.565A2 2 0 0 1 9.935 11.5H13.5a.5.5 0 0 1 0 1H9.935a1.999 1.999 0 0 1-3.87 0H2.5a.5.5 0 0 1 0-1h3.565A2 2 0 0 1 7.5 10.065V2.5A.5.5 0 0 1 8 2Zm0 9a1 1 0 1 0 1 1l-.005-.102A1 1 0 0 0 8 11Z" />
+    </svg>
+  );
+}
