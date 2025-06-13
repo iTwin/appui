d86a3c1f7ffcc0cf1882a13b404708fa3cdcedec
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-06-13 15:52:47 +0300

    remove-docs-autodocs migration

--- docs/storybook/.storybook/main.ts
+++ docs/storybook/.storybook/main.ts
@@ -7,19 +7,18 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
   viteFinal: async (config) => {
     if (!config.build) config.build = {};
     config.build.chunkSizeWarningLimit = 5000;
     // This prevents component name mangling in stories.
     config.build.minify = false;
     return config;
-  },
+  }
 };
 export default config;
