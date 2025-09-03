98c1e2e0b3abcef8e5b8d08ed0ba5b5f314f0f84
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-09-03 11:49:42 +0300

    Update WidgetState docs.

--- ui/appui-react/src/appui-react/widgets/WidgetState.ts
+++ ui/appui-react/src/appui-react/widgets/WidgetState.ts
@@ -6,13 +6,31 @@
  * @module Widget
  */
 
-/** Widget state enum.
+/**
+ * Describes the visibility and mounting state of a widget.
  * @public
  */
 export enum WidgetState {
+  /**
+   * Both the widget tab and its content are visible.
+   */
   Open = 0,
+  /**
+   * The widget tab is visible, but the widget content is not visible.
+   */
   Closed = 1,
+  /**
+   * Neither the widget tab nor its content are visible, but the widget's React node is still rendered.
+   * Useful for pre-initialization and preserving widget state while the widget is not visible.
+   */
   Hidden = 2,
+  /**
+   * Widget is in a floating state. Widget content visibility depends on internal logic, but the widget tab is visible.
+   */
   Floating = 3,
+  /**
+   * Neither the widget tab nor its content are visible, and the widget's React node is not rendered.
+   * Changing the widget state to `Unloaded` will unmount the content node if it was previously rendered.
+   */
   Unloaded = 4,
 }
