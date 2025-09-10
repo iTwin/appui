1b517c1e289dcd52525b34262ed37a5689445315
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-09-10 16:39:37 +0300

    Simplify SplitViewport story

--- docs/storybook/src/frontstage/SplitViewport.stories.tsx
+++ docs/storybook/src/frontstage/SplitViewport.stories.tsx
@@ -5,11 +5,9 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import {
   ConditionalBooleanValue,
-  ConditionalStringValue,
   StandardContentLayouts,
 } from "@itwin/appui-abstract";
 import {
-  ContentProps,
   IModelViewportControl,
   SyncUiEventId,
   ToolbarItemUtilities,
@@ -17,15 +15,7 @@ import {
   ToolbarUsage,
   UiFramework,
 } from "@itwin/appui-react";
-import { ConditionalIconItem } from "@itwin/core-react";
-import {
-  Svg2D,
-  Svg3D,
-  SvgAirplane,
-  SvgClipboard,
-  SvgWindow,
-  SvgWindowSplitVertical,
-} from "@itwin/itwinui-icons-react";
+import { SvgWindow, SvgWindowSplitVertical } from "@itwin/itwinui-icons-react";
 import { AppUiDecorator } from "../Decorators";
 import { Page } from "../AppUiStory";
 import { createFrontstage, removeProperty } from "../Utils";
@@ -51,31 +41,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const contentPropsArray: ContentProps[] = [];
-contentPropsArray.push({
-  id: "imodel-view-0",
-  classId: IModelViewportControl.id,
-});
-contentPropsArray.push({
-  id: "imodel-view-1",
-  classId: IModelViewportControl.id,
-});
-
-let leftViewportActive = false;
-const testIcon1 = () => {
-  return leftViewportActive ? <SvgWindowSplitVertical /> : <SvgWindow />;
-};
-const testIcon2 = () => {
-  return leftViewportActive ? <Svg2D /> : <Svg3D />;
-};
-const testIcon3 = () => {
-  return leftViewportActive ? <SvgClipboard /> : <SvgAirplane />;
-};
-
-UiFramework.content.onActiveContentChangedEvent.addListener(() => {
-  leftViewportActive = !leftViewportActive;
-});
-
 export const Default: Story = {
   args: {
     frontstages: [
@@ -83,7 +48,16 @@ export const Default: Story = {
         contentGroupProps: {
           id: "split-vertical-group",
           layout: StandardContentLayouts.twoVerticalSplit,
-          contents: contentPropsArray,
+          contents: [
+            {
+              id: "imodel-view-0",
+              classId: IModelViewportControl.id,
+            },
+            {
+              id: "imodel-view-1",
+              classId: IModelViewportControl.id,
+            },
+          ],
         },
       }),
     ],
@@ -91,84 +65,42 @@ export const Default: Story = {
       {
         id: "toolbar",
         getToolbarItems: () => {
+          const layouts = {
+            standard: {
+              usage: ToolbarUsage.ContentManipulation,
+              orientation: ToolbarOrientation.Horizontal,
+            },
+          };
           return [
-            ...getToolbarItems(
-              ToolbarUsage.ContentManipulation,
-              ToolbarOrientation.Horizontal
-            ),
-            ...getToolbarItems(
-              ToolbarUsage.ContentManipulation,
-              ToolbarOrientation.Vertical
-            ),
-            ...getToolbarItems(
-              ToolbarUsage.ViewNavigation,
-              ToolbarOrientation.Horizontal
-            ),
-            ...getToolbarItems(
-              ToolbarUsage.ViewNavigation,
-              ToolbarOrientation.Vertical
-            ),
+            ToolbarItemUtilities.createActionItem({
+              id: "action1",
+              label: "Action 1",
+              icon: <SvgWindow />,
+              isHidden: new ConditionalBooleanValue(
+                () =>
+                  UiFramework.content.getActiveId() === "imodel-view-0"
+                    ? false
+                    : true,
+                [SyncUiEventId.ActiveContentChanged]
+              ),
+              layouts,
+            }),
+            ToolbarItemUtilities.createActionItem({
+              id: "action2",
+              label: "Action 2",
+              icon: <SvgWindowSplitVertical />,
+              isHidden: new ConditionalBooleanValue(
+                () =>
+                  UiFramework.content.getActiveId() === "imodel-view-1"
+                    ? false
+                    : true,
+                [SyncUiEventId.ActiveContentChanged]
+              ),
+              layouts,
+            }),
           ];
         },
       },
     ],
   },
 };
-
-function getToolbarItems(usage: ToolbarUsage, orientation: ToolbarOrientation) {
-  const layouts = {
-    standard: {
-      usage,
-      orientation,
-    },
-  };
-  return [
-    ToolbarItemUtilities.createActionItem(
-      `Test1and2`,
-      0,
-      new ConditionalIconItem(testIcon1, [SyncUiEventId.ActiveContentChanged]),
-      new ConditionalStringValue(
-        () => (leftViewportActive ? "Test 1" : "Test 2"),
-        [SyncUiEventId.ActiveContentChanged]
-      ),
-      () => undefined,
-      {
-        layouts,
-      }
-    ),
-    ToolbarItemUtilities.createActionItem(
-      `Test3and4`,
-      1,
-      new ConditionalIconItem(testIcon2, [SyncUiEventId.ActiveContentChanged]),
-      new ConditionalStringValue(
-        () => (leftViewportActive ? "Test 3" : "Test 4"),
-        [SyncUiEventId.ActiveContentChanged]
-      ),
-      () => undefined,
-      {
-        isDisabled: new ConditionalBooleanValue(
-          () => (leftViewportActive ? true : false),
-          [SyncUiEventId.ActiveContentChanged]
-        ),
-        layouts,
-      }
-    ),
-    ToolbarItemUtilities.createActionItem(
-      `Test5and6`,
-      2,
-      new ConditionalIconItem(testIcon3, [SyncUiEventId.ActiveContentChanged]),
-      new ConditionalStringValue(
-        () => (leftViewportActive ? "Test 5" : "Test 6"),
-        [SyncUiEventId.ActiveContentChanged]
-      ),
-      () => undefined,
-      {
-        isHidden: new ConditionalBooleanValue(
-          () => (leftViewportActive ? true : false),
-          [SyncUiEventId.ActiveContentChanged]
-        ),
-        layouts,
-      }
-    ),
-  ];
-}
