4b99261ce859197d3665b6399f0308b953196992
Jean-Francois Corriveau <jean-francois.corriveau@bentley.com>
2025-09-30 09:34:48 -0400

    missing unit test

--- ui/appui-react/src/test/statusfields/usePosition.test.tsx
+++ ui/appui-react/src/test/statusfields/usePosition.test.tsx
@@ -12,4 +12,13 @@ describe("usePosition", () => {
     const { result } = renderHook(() => usePosition(refElement));
     expect(result.current.position.current).toEqual({ left: 100, bottom: 200 });
   });
+  it('should update position on window resize', () => {
+    const mockGetBoundingClientRect = vitest.fn(() => ({ left: 100, bottom: 200 }));
+    const refElement = { current: { getBoundingClientRect: mockGetBoundingClientRect } } as unknown as React.RefObject<HTMLElement>;
+    const { result } = renderHook(() => usePosition(refElement));
+    expect(result.current.position.current).toEqual({ left: 100, bottom: 200 });
+    mockGetBoundingClientRect.mockReturnValue({ left: 150, bottom: 250 });
+    window.dispatchEvent(new Event("resize"));
+    expect(result.current.position.current).toEqual({ left: 150, bottom: 250 });
+  });
 });
