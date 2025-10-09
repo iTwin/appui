f8272b5433aec9ce6f6dc2f796c949a2a8dae226
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-10-09 16:34:46 +0300

    Make docs happy

--- ui/appui-react/src/appui-react/accudraw/AccuDrawStore.tsx
+++ ui/appui-react/src/appui-react/accudraw/AccuDrawStore.tsx
@@ -2,6 +2,10 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module AccuDraw
+ */
+
 import { create } from "zustand";
 
 interface AccuDrawStore {
