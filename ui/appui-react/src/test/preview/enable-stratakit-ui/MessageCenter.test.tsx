dd2b6199df9a48d2f1257fd70fbf92e7f88bf58d
Jean-Francois Corriveau <jean-francois.corriveau@bentley.com>
2025-10-22 16:40:27 -0400

    fix warning in unit test

--- ui/appui-react/src/test/preview/enable-stratakit-ui/MessageCenter.test.tsx
+++ ui/appui-react/src/test/preview/enable-stratakit-ui/MessageCenter.test.tsx
@@ -3,16 +3,17 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { MessageCenterFieldSk, MessageManager } from "../../../appui-react.js";
 import { userEvent, waitForPosition } from "../../TestUtils.js";
 import { Root } from "@stratakit/foundations";
+import { ThemeProvider } from "@itwin/itwinui-react";
 
 function setup({ children }: { children: React.ReactNode }) {
   return (
-    <Root colorScheme="dark" density="dense">
+    <ThemeProvider as={Root} density="dense" theme="dark" colorScheme="dark">
       {children}
-    </Root>
+    </ThemeProvider>
   );
 }
 
@@ -21,6 +22,19 @@ describe(`MessageCenter with Stratakit`, () => {
 
   beforeEach(() => {
     theUserTo = userEvent.setup();
+
+    // Mock window.matchMedia
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: vitest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: vitest.fn(),
+        removeEventListener: vitest.fn(),
+        dispatchEvent: vitest.fn(),
+      })),
+    });
   });
 
   it("Message Center should close on outside click", async () => {
@@ -47,7 +61,9 @@ describe(`MessageCenter with Stratakit`, () => {
   it("Message Center should open on OpenMessageCenterEvent", async () => {
     render(setup({ children: <MessageCenterFieldSk /> }));
     expect(screen.queryByRole("dialog")).toEqual(null);
-    MessageManager.onOpenMessageCenterEvent.emit({});
+
+    act(() => MessageManager.onOpenMessageCenterEvent.emit({}));
+
     expect(await screen.findByRole("dialog")).to.exist;
   });
 });
