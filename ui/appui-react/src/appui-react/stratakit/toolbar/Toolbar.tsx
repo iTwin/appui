cd41898087d45aaaf2ed0fecb37cb5d4f877847c
GerardasB <10091419+GerardasB@users.noreply.github.com>
2025-10-30 14:56:06 +0200

    Initial StrataKit toolbar

---
+++ ui/appui-react/src/appui-react/stratakit/toolbar/Toolbar.tsx
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Toolbar
+ */
+
+import * as React from "react";
+import type { ToolbarProps } from "../../toolbar/Toolbar.js";
+
+/** @internal */
+export function Toolbar(_props: ToolbarProps) {
+  return <>StrataKit toolbar</>;
+}
