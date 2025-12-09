da8ff39b72d2d7e6b2c47f7106e56524972757b6
Omar H. <3652875+arome@users.noreply.github.com>
2025-12-09 09:27:23 -0500

    revert changes to manager and pnpm-lock

---
+++ docs/storybook/.storybook/manager.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { addons } from "storybook/manager-api";
+import theme from "./theme";
+
+addons.setConfig({
+  theme,
+});
