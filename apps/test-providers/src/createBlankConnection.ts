9fcea5a89572db0da227abad6cb20aaf681f7d66
GerardasB <10091419+GerardasB@users.noreply.github.com>
2026-02-25 20:42:10 +0200

    Fix lint

--- apps/test-providers/src/createBlankConnection.ts
+++ apps/test-providers/src/createBlankConnection.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Cartographic, ColorDef, RenderMode } from "@itwin/core-common";
+import { Cartographic, ColorDef } from "@itwin/core-common";
 import {
   BlankConnection,
   IModelApp,
